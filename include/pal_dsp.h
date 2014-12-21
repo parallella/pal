ac94e254408f689b23af015a4d341d52355ef4b4
aolofsson <andreas@adapteva.com>
2014-12-20 21:32:24 -0500

    Common types? Making fft separate

--- include/pal_dsp.h
+++ include/pal_dsp.h
@@ -64,20 +64,6 @@ void p_hist2d_32f();
 /*histogram function*/
 void p_threshold2d_32f();
 
-/*
- * FFT Routines (power of 2 only)
- * (64,128,256,512,1024,2048,4096,8192,16384,32768,65536)
- *
- */
-
-/*Set up FFT plan*/
-p_fftplan_32f(xx, int nx, int ny, int nz, int flags);
-
-/*Execute FFT*/
-p_fftexec_32f(xx, float *datain, float *dataout, int flags);
-
-/*Tear down FFT plan*/
-p_fftdestroy(xx);
 
 
 
--- include/pal_fft.h
+++ include/pal_fft.h
@@ -1,7 +1,16 @@
 #include <complex.h>
 
+/*
+ * FFT Routines (power of 2 only to start with)
+ * (64,128,256,512,1024,2048,4096,8192,16384,32768,65536)
+ *
+ */
 
-/*complex 1D FFT*/
-void p_cfft (float* complex a, int n, int isign, float* wsave);
+/*Set up FFT plan*/
+p_fftplan_32f(xx, int nx, int ny, int nz, int flags);
 
+/*Execute FFT*/
+p_fftexec_32f(xx, float *datain, float *dataout, int flags);
 
+/*Tear down FFT plan*/
+p_fftdestroy(xx);
