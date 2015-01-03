8b78cb6f8ed843ed99ae9b5c5fcab32fc3a3d9b4
aolofsson <andreas@adapteva.com>
2015-01-02 23:16:28 -0500

    Removing extra argument from function

--- image/p_median3x3.c
+++ image/p_median3x3.c
@@ -1,20 +1,9 @@
 /*
- * A median 3x3 filter
+ * A median 3x3 filter 
  *
- *       | -1  0  1 |
- * Gx =  | -2  0  2 | * 1/8
- *       | -1  0  1 |
- *
- *       | -1 -2 -1 |
- * Gy =  |  0  0  0 | * 1/8
- *       |  1  2  1 |
- *
- * G = sqrt (Gx^2 + Gy^2)
- * 
- * Gradient Direction (theta) = atan2(Gy,Gx)
  *
  */
-void p_median3x3_32f (const float* x, const float* m, float* r, int rows, int cols){
+void p_median3x3_32f (const float* x, float* r, int rows, int cols){
 
 
 /*PLACE CODE HERE*/
