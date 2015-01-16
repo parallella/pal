1c3dac3f5732743bdca52bc264276c5deb6cfa3a
aolofsson <andreas@adapteva.com>
2015-01-15 23:49:20 -0500

    Starting to fill out the structures

--- core/pal_core_private.h
+++ core/pal_core_private.h
@@ -1,35 +1,31 @@
 #include "pal_core.h"
+#define N 4096
 
 struct pal_dev {
-
-    int name;
-    int whoami;
-    int topology;
-    int nodes;
-    int origin;
-    int rows;
-    int cols;
-    int chip_cols;
-    int chip_rows;
-    int loc_memsize;
-    int sh_memsize;
+    int properties[32];   
 };
 
-struct pal_team {
-    int origin;
-    int rows;
-    int cols;
-};
-
-struct pal_symbol {
-
+struct pal_team {    
+    struct pal_dev* dev;         
+    u32    proc_bitmap[N/32]; //holds one bit per processor
+    //A lot less efficient to make it generic rather than block
+    //based
 };
 
 struct pal_mem {
-
+    struct pal_dev* dev;
+    size_t memsize;
+    void* mem;
+    
 };
 
 struct pal_program {
+    struct pal_dev* dev;
+    size_t progsize;
+    void* program;
+};
+
+struct pal_symbol {
 
 };
 
