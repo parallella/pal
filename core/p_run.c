7c343cfbb6c52fd7f8dec764c51ef5967ff19094
aolofsson <andreas@adapteva.com>
2015-01-16 22:58:37 -0500

    Need a dynamic array of team members. Everything starts with size.

--- core/p_run.c
+++ core/p_run.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "pal_core.h"
 #include "pal_core_private.h"
 int p_run(p_program_t *prog, p_team_t *team, int argn, void **args, int flags){
@@ -5,7 +6,10 @@ int p_run(p_program_t *prog, p_team_t *team, int argn, void **args, int flags){
     int i, id;
         
     for(i=0;i<(team->size);i++){
+	printf("Running program on core %d\n",i);
 	//launch program to node "id"
+	//need to extend to treat an arbitrary size list of IDs
+	//don't assume contiugous arrays! 
     }
     return(0);
 }
