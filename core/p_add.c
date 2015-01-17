890e4bf368c90accb4ac436a2662942bc6b1835a
aolofsson <andreas@adapteva.com>
2015-01-16 21:39:22 -0500

    Adding add/remove member functions Making the open function only reserve initial structure space Advanced users can add members, many will only need to add one array

---
+++ core/p_add.c
@@ -0,0 +1,7 @@
+#include "pal_core.h"
+#include "pal_core_private.h"
+
+int p_add (p_team_t *team, int start, int size){
+    
+    return(0);
+}
--- core/p_open.c
+++ core/p_open.c
@@ -1,11 +1,22 @@
 #include "pal_core.h"
 #include "pal_core_private.h"
 
-void *p_open (p_dev_t *dev, int start, int total){
-
+void *p_open (p_dev_t *dev, int start, int size){
+    
     p_team_t *team;
-    
-    /*PLACE CODE HERE*/   
-    
+
+    team=(p_team_t *) malloc(sizeof(p_team_t));
+    team->dev   = dev;  //setting pointer to device structure
+    team->size  = size;//setting size of team 
+
+    //An array of team members, every member has:
+    //1.a processor ID (integer)
+    //2.a busy flag (gets set on launched, cleard on program completion)
+    //3.Or the slow solution is to query the remote machines... 
+    //  grid-->look to see if process exited on all machines
+    //  epiphany-->poll the idle state
+    //  smp-->see if process exited on all machines
+    //4. what else?
+
     return(team);
 }
