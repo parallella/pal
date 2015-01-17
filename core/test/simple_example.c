4627efa3601cd67041f8969002cbd45b74c1ea24
aolofsson <andreas@adapteva.com>
2015-01-16 20:37:30 -0500

    Changed the simple example again... Final answer, pointers to objects are return arguments.. Memory allocation is done inside the function call.. User just defines the pointers (on the stack)...

--- core/test/simple_example.c
+++ core/test/simple_example.c
@@ -1,40 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <pal_core.h>
 
 int main(int argc, char *argv[]){
+      
+    p_dev_t *dev0;       //device information object
+    p_program_t *prog0;  //in memory exetutable object
+    p_team_t *team0;     //working team object
+    p_mem_t *mem0;       //memory object
 
-    //Opaque objects	   
-    p_dev_t dev0; 
-    p_program_t prog0;
-    p_team_t team0;
-    
-    //Local variables
-    int total;   
+    int status;
+    int nodes;
 
-    //Initialize system
-    p_init(EPIPHANY, DEFAULT, dev0);
-   
-    //Load an ELF file from the file system
-    p_load(dev0, "./hello.elf", prog0);
-
-    //Dynamically create a team
-    p_query(dev0, TOTAL, &total); 
-  
-    //Open a team (additive)
-    p_open(dev0, 0, total, team0);
-    
-    //Run the program "process" on a team of processors
-    int nargs = 0;
-    void*  args[]={};	      	
-    p_run(prog0,team0,nargs, args, ASYNC);
-
-    //Wait until team has finished work     
-    p_barrier(team0);
-
-    //Close down the team
-    p_close(team0);
-
-    //Close down device
-    p_finalize(dev0);    
+    dev0   = p_init(EPIPHANY, STANDARD);    //initialize system
+    prog0  = p_load(dev0, "./hello.elf");   //load executable file into memory
+    nodes  = p_query(dev0, NODES);          //query how many nodes are in system
+    team0  = p_open(dev0, 0, nodes);        //Open a team (additive)
+    status = p_run(prog0, team0, 0, NULL, ASYNC); //run program on team
+    status = p_barrier(team0);              //set barrier on work team
+    status = p_free(team0);                //free the resource)anything with po
+    status = p_finalize(dev0);             //close down the device    
 }
