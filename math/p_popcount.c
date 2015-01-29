3cc531f5ba3c3ff943d33fc94ca94ef4b19230d2
aolofsson <andreas@adapteva.com>
2015-01-29 01:00:15 -0500

    Adding sort and popcount functions

---
+++ math/p_popcount.c
@@ -0,0 +1,16 @@
+/**
+ *
+ * Counts the number of bits set in 'a'. 
+ *
+ * @param a     Pointer to input vector
+ *
+ * @param c     Pointer to result vector
+ *
+ * @param n     Size of 'a' and 'c' vector.
+ *       
+ * @return      None
+ *
+ */
+void p_popcount_u32 (unsigned int* a, unsigned int* c){
+
+}
---
+++ math/p_sort.c
@@ -0,0 +1,20 @@
+/**
+ *
+ * Sorts an array of values
+ *
+ * @param a     Pointer to input vector
+ *
+ * @param c     Pointer to result vector
+ *
+ * @param n     Size of 'a' and 'c' vector.
+ *       
+ * @return      None
+ *
+ */
+void p_sort_32f (float *a, float* c, int n){
+
+}
+
+void p_sort_u32 (int *a, int* c, int n){
+    
+}
