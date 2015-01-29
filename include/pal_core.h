8842a572854d793636d216a0de9544f86c040cd6
aolofsson <andreas@adapteva.com>
2015-01-28 23:22:40 -0500

    File descriptor header change: -global tables (for now) -gather/scatter api cleanup -argument changes to clarify

--- include/pal_core.h
+++ include/pal_core.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+
 /*
  ***********************************************************************
  * SPECIFIC TYPES (LINUX INSPIRED)
@@ -26,6 +27,7 @@ typedef uint8_t u8;
 #define SMP       0x03
 #define FPGA      0x04
 #define GPU       0x05
+#define DEMO      0x06
 
 /*
  ***********************************************************************
@@ -69,6 +71,17 @@ typedef uint8_t u8;
  ***********************************************************************
  */
 
+typedef struct p_dev_table p_dev_table_t;
+typedef struct p_team_table p_team_table_t;
+typedef struct p_program_table p_program_table_t;
+typedef struct p_mem_table p_mem_table_t;
+typedef struct p_dev p_dev_t;
+typedef struct p_team p_team_t;
+typedef struct p_program p_program_t;
+typedef struct p_symbol p_symbol_t;
+typedef struct p_event p_event_t;
+typedef struct p_mem p_mem_t;
+typedef struct p_memptr p_memptr_t;
 typedef struct p_atom p_atom_t;
 typedef struct p_mutex p_mutex_t;
 typedef struct p_mutex_attr p_mutex_attr_t;
@@ -79,14 +92,11 @@ typedef struct p_mutex_attr p_mutex_attr_t;
  ***********************************************************************
  */
 
-/*Device structure setup*/
+/*Initialize device run time*/
 int p_init(int type, int flags);
 
-/*Query a property of a device*/
-int p_query(int dev, int property);
-
-/*Loads a program from the file system into memory */
-int p_load(int dev, char *file, char *function);
+/*Finalize device run time*/
+int p_finalize(int dev);
 
 /*Open a team of processors*/
 int p_open(int dev, int start, int count);
@@ -97,30 +107,37 @@ int p_append(int team, int start, int count);
 /*Remove team members*/
 int p_remove(int team, int start, int count);
 
-/*Run a program on N processors*/
-int p_run(int prog, int team, int start, int count, 
-	  int argn, void *args[], int flags);
-
 /*Close a team of processors*/
 int p_close(int team); 
+
+/*Loads a program from the file system into memory */
+int p_load(int dev, char *file, char *function, int flags);
+
+/*Run a program on N processors*/
+int p_run(int prog, int team, int start, int count, 
+	  int nargs, void *args[], int flags);
 
 /*Execution barrier*/
 int p_barrier(int team);
 
-/*Local memory allocation*/
+/*Create a local memory object*/
 int p_malloc(int team, size_t size);
 
-/*Global memory allocation*/
-int p_gmalloc(int team, int n, size_t size);
+/*Create a remote memory object*/
+int p_rmalloc(int team, int pid, size_t size);
 
 /*Free allocated memory */
 int p_free(int mem);
 
-/*Finalize device run time*/
-int p_finalize(int dev);
-
 /*Memory fence*/
 int p_fence(int mem);
+
+/*Flushes the read and write paths to a specific memory object*/
+ssize_t p_flush(int mem);
+
+/*Query a property of a device*/
+/*need it for mem, team, prog as well?*/
+int p_devquery(int dev, int property);
 
 /*
  ***********************************************************************
@@ -129,25 +146,26 @@ int p_fence(int mem);
  */
 
 /*Writes to a global memory address from a local address*/
-ssize_t p_write(int mem, const void *src, size_t nbytes, int flags);
+ssize_t p_write(int mem, const void *src, size_t nb, int flags);
 
 /*Reads from a global memory address */
-ssize_t p_read(int mem, void *dst, size_t nbytes, int flags);    
-
-/*Flushes the read and write paths to a specific memory object*/
-ssize_t p_flush(int mem);
-
-/*Scatters data from a local array to a list of remote memory objects*/
-ssize_t p_scatter(int *mlist[], void *src, size_t nsrc, size_t ndst, int flags); 
-
-/*Gathers data from a list of remote memory objects into a local array*/
-ssize_t p_gather(int *mlist[], void *dst, size_t nsrc, size_t ndst, int flags); 
+ssize_t p_read(int mem, void *dst, size_t nb, int flags);    
 
 /*Broadcasts an array based to a list of destination pointers*/
-ssize_t p_broadcast(int *mlist[], void *src, size_t nsrc, size_t ndst, int flags); 
+ssize_t p_broadcast(int *mlist[], int mcount, void *src, size_t nb, int flags);
+
+/*Scatters data from a local array to a list of remote memory objects*/
+ssize_t p_scatter(int *mlist[], int mcount, 
+		  void *suf, size_t scount,
+		  int disp[], int flags); 
+
+/*Scatters data from a local array to a list of remote memory objects*/
+ssize_t p_gather(int *mlist[], int mcount, 
+		 void *dbuf, size_t dcount,
+		 int disp[], int flags); 
 
 /*Specialized low level shared memory memcpy interface (non-blocking)*/
-ssize_t p_memcpy(void *dst, void *src, size_t nbytes, int flags);
+ssize_t p_memcpy(void *dst, void *src, size_t nb, int flags);
 
 /*
  ***********************************************************************
@@ -195,3 +213,31 @@ int p_atomic_swap_u32(p_atom_t *atom, u32 *input);
 int p_atomic_compswap_u32(p_atom_t *atom, u32 *input, u32 expected);
 
 #endif
+
+/*
+ ***********************************************************************
+ * Global system variables (will be replaced with O/S real time
+ ***********************************************************************
+ */
+
+struct p_dev_table {
+    p_dev_t *devptr[16];
+    int size;
+};
+struct p_team_table {
+    p_team_t *teamptr[16];
+    int size;
+};
+struct p_program_table {
+    p_program_t *progptr[16];
+    int size;
+};
+struct p_mem_table {
+    p_mem_t *memptr[16];
+    int size;
+};
+
+struct p_dev_table p_dev_table_global;
+struct p_team_table p_team_table_global;
+struct p_program_table p_program_table_global;
+struct p_mem_table p_mem_table_global;
