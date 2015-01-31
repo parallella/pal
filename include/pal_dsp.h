3883402ce083286888694d98ace499bc9186b9c0
aolofsson <andreas@adapteva.com>
2015-01-31 17:15:18 -0500

    Fixing up argument order for dsp functions

--- include/pal_dsp.h
+++ include/pal_dsp.h
@@ -12,31 +12,28 @@
  */
 
 /*auto correlation: r[j] = sum ( x[j+k] * x[k] ), k=0..(n-j-1) */
-void p_acorr_f32(float* x, float* r, int nx );
+void p_acorr_f32(float* x, int nx, float* r);
 
 /*convolution: r[j] = sum ( h[k] * x[j-k), k=0..(nh-1) */
-void p_conv_f32(float* x, float* h, float* r, int nx, int nh );
+void p_conv_f32(float* x, float* h, int nx, int nh, float* r);
 
 /*correlation: r[j] = sum ( x[j+k] * y[k]), k=0..(nx+ny-1) */
-void p_corr_f32(float* x, float* y, float* r, int nx, int ny );
+void p_xcorr_f32(float* x, float* y, int nx, int ny, float* r );
 
 /*FIR filter direct form: r[j] = sum ( h[k] * x [j-k]), k=0..(nh-1) */
-void p_fir_f32(float* x, float* h, float* r, float* dbuf, int nx, int nh );
+void p_fir_f32(float* x, float* h, float* dbuf, int nx, int nh, float* r );
 
 /*FIR filter with decimation: r[j] = sum ( h[k] * x [j*D-k]), k=0..(nh-1) */
-int p_firdec_f32(float* x, float* h, float* r, float* dbuf, int nx, int nh, int dfactor );
+int p_firdec_f32(float* x, float* h, float* dbuf, int nx, int nh, int d, float* r);
 
 /*FIR filter with inerpolation: r[j] = sum ( h[k] * x [j*D-k]), k=0..(nh-1) */
-void p_firint_f32(float* x, float* h, float* r, float* b, int nx, int nh, int ifr );
-
-/*FIR filter lattice form*/
-void p_firlat_f32(float* x, float* h, float* r, float* dbuf, int nx, int nh);
+void p_firint_f32(float* x, float* h,  float* b, int nx, int nh, int ifactor, float* r);
 
 /*FIR symmetric form: */
-void p_firsym_f32(float* x, float* h, float* r, float* dbuf, int nx, int nh);
+void p_firsym_f32(float* x, float* h, float* dbuf, int nx, int nh, float* r);
 
 /*IIR filter: */
-void p_iir_f32(float* x, float* h, float* r, float* dbuf, int nx, int nh);
+void p_iir_f32(float* x, float* h, float* dbuf, int nx, int nh, float* r);
 
 
 
