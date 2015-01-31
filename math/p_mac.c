634394ca06fe1a02d6765edba0ce9912a1e2c9bb
aolofsson <andreas@adapteva.com>
2015-01-31 11:46:40 -0500

    Changing madd to "mac" (feels more natural for dsp)

---
+++ math/p_mac.c
@@ -0,0 +1,24 @@
+/**
+ *
+ * Element wise multiply accumulate: c += a * b' 
+ *
+ * @param a     Pointer to input vector
+ *
+ * @param b     Pointer to input vector
+ *
+ * @param c     Pointer to output vector
+ *
+ * @param n     Size of 'a' and 'c' vector.
+ *       
+ * @return      None
+ *
+ */
+
+void p_mac_32f (float* a, float* b,  float* c, int n ){
+
+    int i;
+    for(i=0;i<n;i++){
+        *(c+i) += *(a+i) * *(b+i);
+    }
+
+}
