f4650f60d3ea8cc32bdd803e8f63878a84741667
Ola Jeppsson <ola@adapteva.com>
2015-02-11 19:35:06 +0100

    pal_core_private.h: Remove ptr suffix for pointers
    You can tell from the type if it is a pointer...
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- core/pal_core_private.h
+++ core/pal_core_private.h
@@ -1,30 +1,32 @@
-//TEMPORARY HACK!!
+#include <errno.h>
+#include <stdint.h>
 
+//TEMPORARY HACK!!
 struct p_dev {
-    int property[32];   
+    int property[32];
 };
 
-struct p_team {    
-    p_dev_t *devptr;   //pointer to the associated device structure
-    int      size;     //number of member nodes    
-    int     *teamptr;  //list of addresses, one per node (int)
-    int     *statptr;  //list of status "regs". idle/working
+struct p_team {
+    p_dev_t  *dev;  //pointer to the associated device structure
+    int      size;  //number of member nodes
+    p_team_t *team; //list of addresses, one per node (int)
+    p_team_t *stat; //list of status "regs". idle/working
 };
 
 //Optimize later, focus on function...
 struct p_mem {
-    p_dev_t *teamptr; //pointer to the associated team structure
-    int      mutex;   //optional mutex to grab before reading/writing 'mem' 
-    int      takeit;  //indicates that new data is ready (wraparound impl)
-    int      gotit;   //indicates that data was read (wraparound impl)
-    int      pilot;   //temp var used for flushing read/write path to 'mem'
-    size_t   size;    //size of memory buffer
-    void    *memptr;  //pointer to allocated memory    
+    p_team_t *team;  //pointer to the associated team structure
+    int      mutex;  //optional mutex to grab before reading/writing 'mem'
+    int      takeit; //indicates that new data is ready (wraparound impl)
+    int      gotit;  //indicates that data was read (wraparound impl)
+    int      pilot;  //temp var used for flushing read/write path to 'mem'
+    size_t   size;   //size of memory buffer
+    void     *mem;   //pointer to allocated memory
 };
 
 struct p_program {
-    p_dev_t *devptr;   //pointer to the associated device structure        
-    char    *name;     //executable file name
+    p_dev_t *dev;   //pointer to the associated device structure
+    char    *name;  //executable file name
 };
 
 struct p_symbol {
