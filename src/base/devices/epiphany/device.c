c9439ab6344af906b313fbcba227b60cf8b948aa
Ola Jeppsson <ola@adapteva.com>
2015-02-17 20:38:54 +0100

    epiphany: Template out all device operations
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- src/base/devices/epiphany/device.c
+++ src/base/devices/epiphany/device.c
@@ -2,4 +2,35 @@
 #include "pal_base.h"
 #include "../../pal_base_private.h"
 
-struct dev_ops __pal_dev_epiphany_ops = { NULL };
+static p_dev_t dev_init(struct dev *dev, int flags)
+{
+    return p_ref_err(ENOSYS);
+}
+
+static void dev_fini(struct dev *dev)
+{
+}
+
+static int dev_query(struct dev *dev)
+{
+    return -ENOSYS;
+}
+
+static struct team *dev_open(struct dev *dev)
+{
+    return p_ref_err(ENOSYS);
+}
+
+static int dev_wait(struct dev *dev, struct team *team)
+{
+    return -ENOSYS;
+}
+
+struct dev_ops __pal_dev_epiphany_ops = {
+    .init = dev_init,
+    .fini = dev_fini,
+    .query = dev_query,
+    .open = dev_open,
+    .wait = dev_wait
+};
+
