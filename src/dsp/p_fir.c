5a80a343ca5c63ee8b64e66aacd6c0ce22064ee9
Andreas Olofsson <andreas@adapteva.com>
2015-03-07 14:06:13 -0500

    Adding optimized FIR function -based on yaniv's old code
    TODO: make delay line dynamic + test correctness
    
    Signed-off-by: Andreas Olofsson <andreas@adapteva.com>

--- src/dsp/p_fir.c
+++ src/dsp/p_fir.c
@@ -25,9 +25,51 @@
  */
 
 void p_fir_f32(float *x, float *h, float *r, int nx, int nh,
-	       int p, p_team_t team)
-{
+	       int p, p_team_t team){
 
-    /*PLACE CODE HERE*/
+    int wrp; // delay line's current position.
+    int rdp; // I/O data's current position
+    int cp;  // FIR coefficient index
+    int dlp; // delay line position per-tap.
+    
+    float dl[64 * 2];   // TODO: Fix size, make dynamic
+    float fir[8];       // temp fir accumulators
+    
+    for (cp=0; cp < nh; cp++){
+	dl[cp] = dl[cp+nh] = 0.0;
+    }   
+    for (rdp=0; rdp<nx; rdp+=nh){
+	for (wrp=0; wrp<nh; wrp+=8){
+	    dl[wrp+0] = dl[wrp+nh+0] = x[rdp+wrp+0];
+	    dl[wrp+1] = dl[wrp+nh+1] = x[rdp+wrp+1];
+	    dl[wrp+2] = dl[wrp+nh+2] = x[rdp+wrp+2];
+	    dl[wrp+3] = dl[wrp+nh+3] = x[rdp+wrp+3];
+	    dl[wrp+4] = dl[wrp+nh+4] = x[rdp+wrp+4];
+	    dl[wrp+5] = dl[wrp+nh+5] = x[rdp+wrp+5];
+	    dl[wrp+6] = dl[wrp+nh+6] = x[rdp+wrp+6];
+	    dl[wrp+7] = dl[wrp+nh+7] = x[rdp+wrp+7];
+	    fir[0] = fir[1] = fir[2] = fir[3] = 0;
+	    fir[4] = fir[5] = fir[6] = fir[7] = 0;
+	    //Unrolled inner loop
+	    for (cp=0, dlp=(wrp+1); cp<nh; cp++, dlp++){
+		fir[0] += h[cp] * dl[dlp + 0];
+		fir[1] += h[cp] * dl[dlp + 1];
+		fir[2] += h[cp] * dl[dlp + 2];
+		fir[3] += h[cp] * dl[dlp + 3];
+		fir[4] += h[cp] * dl[dlp + 4];
+		fir[5] += h[cp] * dl[dlp + 5];
+		fir[6] += h[cp] * dl[dlp + 6];
+		fir[7] += h[cp] * dl[dlp + 7];
+	    }
+	    r[rdp+wrp+0] = fir[0];
+	    r[rdp+wrp+1] = fir[1];
+	    r[rdp+wrp+2] = fir[2];
+	    r[rdp+wrp+3] = fir[3];
+	    r[rdp+wrp+4] = fir[4];
+	    r[rdp+wrp+5] = fir[5];
+	    r[rdp+wrp+6] = fir[6];
+	    r[rdp+wrp+7] = fir[7];
+	}
+    }    
 }
 
