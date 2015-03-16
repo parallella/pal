e07f330883ff720cf9a5746eea884cc67b2e97eb
Mateusz Kacprzak <mateusz.kacprzak@yandex.ru>
2015-03-16 18:21:33 +0100

    image: p_median3x3: Add function implementation
    Targeting memory traffic efficiency.
    
    Signed-off-by: Mateusz Kacprzak <mateusz.kacprzak@yandex.ru>

--- src/image/p_median3x3.c
+++ src/image/p_median3x3.c
@@ -22,6 +22,41 @@
 void p_median3x3_f32(float *x, float *r, int rows, int cols, 
 		     int p, p_team_t team)
 {
+    float buffer[9];
+    float *px, *pr;
+    int i, j, buffer_col;
 
-    /*PLACE CODE HERE*/
+    px = x;
+    pr = r;
+
+    for (i = 0; i < rows - 2; i++) {
+        // fully filling first window
+        buffer[0] = *px;
+        buffer[1] = *(px + 1);
+        buffer[2] = *(px + 2);
+
+        buffer[3] = *(px + cols);
+        buffer[4] = *(px + cols + 1);
+        buffer[5] = *(px + cols + 2);
+
+        buffer[6] = *(px + cols + cols);
+        buffer[7] = *(px + cols + cols + 1);
+        buffer[8] = *(px + cols + cols + 2);
+
+        p_median_f32(buffer, pr, 9, 0, 0);
+        pr++;
+        px += 3;
+        // other windows differ only by one column
+        // so only one is exchanged
+        for (j = 0; j < cols - 3; j++) {
+            buffer_col = j % 3;
+            buffer[buffer_col] = *px;
+            buffer[buffer_col + 3] = *(px + cols);
+            buffer[buffer_col + 6] = *(px + cols + cols);
+
+            p_median_f32(buffer, pr, 9, 0, 0);
+            pr++;
+            px++;
+        }
+    }
 }
