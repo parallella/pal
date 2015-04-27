f16849640b5798d49b6debba430f179a1cf3ae00
Ola Jeppsson <ola@adapteva.com>
2015-04-27 11:32:31 +0200

    tests: Add basic test infrastructure
    runtest needs to be compiled for the build machine. It is then used to
    kick off tests on the host. For now only support native (simple). Later
    we should use the pal API.
    
    * tests/Makefile.am: New file.
    * tests/runtest.noimpl.c: New file.
    * tests/runtest.simple.c: New file.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

---
+++ tests/runtest.noimpl.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    fprintf(stderr, "WARNING: runtest not implemented for platform yet\n");
+
+    /* Skipped test. see:
+     * https://www.gnu.org/software/automake/manual/html_node/Scripts_002dbased-Testsuites.html
+     */
+    return 77;
+}
