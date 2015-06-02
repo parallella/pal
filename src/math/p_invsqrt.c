a0a173f9119c4a001f2728a39d2aa0bbb2ea9909
Pierre Fenoll <pierrefenoll@gmail.com>
2015-06-02 10:34:44 -0700

    Try optimizing invsqrt with Fast Inverse Square Root

--- src/math/p_invsqrt.c
+++ src/math/p_invsqrt.c
@@ -2,7 +2,7 @@
 
 /**
  *
- * Calculates the inverse square root of the input vector 'a'.
+ * Calculates the inverse square root of the input vector 'a' using Fast Inverse Square Root.
  *
  * @param a     Pointer to input vector
  *
@@ -20,8 +20,17 @@
 #include <math.h>
 void p_invsqrt_f32(float *a, float *c, int n, int p, p_team_t team)
 {
+    long j;
+    float half, y;
+
     int i;
     for (i = 0; i < n; i++) {
-        *(c + i) = 1.0f / sqrtf(*(a + i));
+        x2 = *(a + i) * 0.5F;
+        y  = *(a + i);
+        i  = *(long *) &y;
+        i  = 0x5f3759df - (i >> 1);
+        y  = *(float *) &i;
+        y  = y * (1.5F - half * y * y); // Repeat this line for more accuracy
+        *(c + i) = y;
     }
 }
