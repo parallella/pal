6f005d00280aab60efe8c5965e4cbeba7f9309c1
Michele <mail.foster@gmail.com>
2015-06-03 02:16:26 +0200

    Update p_conv.c

--- src/dsp/p_conv.c
+++ src/dsp/p_conv.c
@@ -25,7 +25,7 @@
 void p_conv_f32(float *x, float *h, float *r, int nx, int nh,
 		int p, p_team_t team)
 {
-  	for ( int i = 0: i < nx; i++)
+  	for ( int i = 0; i < nx; i++)
   	{
   		for(int j = 0; j < nh; j++)
   		{
