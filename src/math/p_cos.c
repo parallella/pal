60e052ee2f797398f92c8d672c6c1689da226045
Matt Thompson <matt.thompson@mandiant.com>
2015-06-02 19:34:24 -0500

    added M_FLOOR and M_NORMALIZE_RADIANS macros. added basic sin and cos functions

--- src/math/p_cos.c
+++ src/math/p_cos.c
@@ -5,6 +5,9 @@
  * Compute the cosine of the vector 'a'. Angles are specified in radians.
  * The radian number must be in the range 0 to 2pi,
  *
+ * Author: Matt Thompson <mthompson@hexwave.com>
+ * Date: Jun 2, 2015
+ *
  * @param a     Pointer to input vector
  *
  * @param c     Pointer to output vector
@@ -18,12 +21,24 @@
  * @return      None
  *
  */
-#include <math.h>
+
+#define COS_ITERATIONS 5
+
 void p_cos_f32(float *a, float *c, int n, int p, p_team_t team)
 {
 
     int i;
     for (i = 0; i < n; i++) {
-        *(c + i) = cosf(*(a + i));
+        float *pa = (a+i);
+        float *pc = (c+i);
+        float val = 1;
+        int k;
+        float theta = M_NORMALIZE_RADIANS(*pa);
+
+        for(k=COS_ITERATIONS-1; k>=0; --k)
+          val = 1 - theta*theta/(2*k+2)/(2*k+1)*val;
+
+        *pc = val;
+
     }
 }
