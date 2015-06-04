60dcb72b5a58060fd80d8a2a129a9290303b8d3f
Tom Anderson <ioot@yahoo.com>
2015-06-04 14:24:27 +0930

    Update p_ftoi.c
    Missing semicolon

--- src/math/p_ftoi.c
+++ src/math/p_ftoi.c
@@ -21,6 +21,6 @@
 void p_ftoi(float *a, int *c, int n, int p, p_team_t team) 
 {
   for(int i = 0; i<n; i++) {
-    *(c+i) = (*(a+i)>=0) ? (int) (*(a+i)+0.5) : (int) (*(a+i)-0.5)
+    *(c+i) = (*(a+i)>=0) ? (int) (*(a+i)+0.5) : (int) (*(a+i)-0.5);
   }
 }
