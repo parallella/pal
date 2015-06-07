6c2509b0e2dc1259171fdd5b2538a577d8c4b14c
algorithm314 <www.kostantinaras@yahoo.gr>
2015-06-07 14:31:51 +0300

    Fast p_inv function without using division
    Signed-off-by: Konstantinos Agiannis <www.kostantinaras@yahoo.gr>

--- src/math/p_inv.c
+++ src/math/p_inv.c
@@ -18,10 +18,22 @@
  *
  */
 #include <math.h>
+
 void p_inv_f32(const float *a, float *c, int n, int p, p_team_t team)
 {
     int i;
+    float cur;
     for (i = 0; i < n; i++) {
-        *(c + i) = 1.0 / *(a + i);
+        cur = *(a + i);
+        union {
+            float f;
+            uint32_t x;
+        } u = {cur};
+		/* First approximation */
+        u.x = 0x7EF312AC - u.x;
+        /* Refine */
+        u.f = u.f * (2 - u.f * cur);
+        u.f = u.f * (2 - u.f * cur);
+        *(c + i) = u.f;
     }
 }
