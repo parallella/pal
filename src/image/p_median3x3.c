16f27d25b79e25fe8f7144c8f1058b532b88edac
Hamid Ebadi <hamid.ebadi@gmail.com>
2015-06-07 14:20:01 +0200

    Fast median network for 9 elements.  QuickSelect method for finding median

--- src/image/p_median3x3.c
+++ src/image/p_median3x3.c
@@ -1,6 +1,46 @@
 #include <pal.h>
 
 /*
+ * The following routines have been built from knowledge gathered
+ * around the Web. I am not aware of any copyright problem with
+ * them, so use it as you want.
+ * N. Devillard - 1998
+ */
+
+typedef float pixelvalue ;
+
+#define PIX_SORT(a,b) { if ((a)>(b)) PIX_SWAP((a),(b)); }
+#define PIX_SWAP(a,b) { pixelvalue temp=(a);(a)=(b);(b)=temp; }
+
+/*----------------------------------------------------------------------------
+   Function :   opt_med9()
+   In       :   pointer to an array of 9 pixelvalues
+   Out      :   a pixelvalue
+   Job      :   optimized search of the median of 9 pixelvalues
+   Notice   :   in theory, cannot go faster without assumptions on the
+                signal.
+                Formula from:
+                XILINX XCELL magazine, vol. 23 by John L. Smith
+  
+                The input array is *NOT* modified in the process
+                The result array is guaranteed to contain the median
+                value
+ ---------------------------------------------------------------------------*/
+
+pixelvalue opt_med9(pixelvalue * pointer)
+{
+    pixelvalue p[9];
+    memcpy(p, pointer, 9*sizeof(pixelvalue) );
+    PIX_SORT(p[1], p[2]) ; PIX_SORT(p[4], p[5]) ; PIX_SORT(p[7], p[8]) ;
+    PIX_SORT(p[0], p[1]) ; PIX_SORT(p[3], p[4]) ; PIX_SORT(p[6], p[7]) ;
+    PIX_SORT(p[1], p[2]) ; PIX_SORT(p[4], p[5]) ; PIX_SORT(p[7], p[8]) ;
+    PIX_SORT(p[0], p[3]) ; PIX_SORT(p[5], p[8]) ; PIX_SORT(p[4], p[7]) ;
+    PIX_SORT(p[3], p[6]) ; PIX_SORT(p[1], p[4]) ; PIX_SORT(p[2], p[5]) ;
+    PIX_SORT(p[4], p[7]) ; PIX_SORT(p[4], p[2]) ; PIX_SORT(p[6], p[4]) ;
+    PIX_SORT(p[4], p[2]) ; return(p[4]) ;
+}
+
+/*
  * A median 3x3 filter.
  *
  * @param x     Pointer to input image, a 2D array of size 'rows' x 'cols'
@@ -19,45 +59,40 @@
  *
  */
 
-void p_median3x3_f32(const float *x, float *r, int rows, int cols,
-                     int p, p_team_t team)
+void p_median3x3_f32(float *x, float *r, int rows, int cols, 
+		     int p, p_team_t team)
 {
     float buffer[9];
-    const float *px;
-    float *pr;
-    int i, j, buffer_col;
+    float sorted[9] ;
+    float *px, *pr;
+    int i, j, buffer_pointer;
 
     px = x;
     pr = r;
 
     for (i = 0; i < rows - 2; i++) {
         // fully filling first window
-        buffer[0] = *px;
-        buffer[1] = *(px + 1);
-        buffer[2] = *(px + 2);
+        buffer[0] = *(px);
+        buffer[1] = *(px + cols);
+        buffer[2] = *(px + cols + cols);
 
-        buffer[3] = *(px + cols);
+        buffer[3] = *(px + 1);
         buffer[4] = *(px + cols + 1);
-        buffer[5] = *(px + cols + 2);
-
-        buffer[6] = *(px + cols + cols);
-        buffer[7] = *(px + cols + cols + 1);
-        buffer[8] = *(px + cols + cols + 2);
-
-        p_median_f32(buffer, pr, 9, 0, 0);
-        pr++;
-        px += 3;
+        buffer[5] = *(px + cols + cols + 1);
+        buffer_pointer = 6 ;
         // other windows differ only by one column
-        // so only one is exchanged
-        for (j = 0; j < cols - 3; j++) {
-            buffer_col = j % 3;
-            buffer[buffer_col] = *px;
-            buffer[buffer_col + 3] = *(px + cols);
-            buffer[buffer_col + 6] = *(px + cols + cols);
-
-            p_median_f32(buffer, pr, 9, 0, 0);
+        // so only one column is added to the place where buffer pointer points
+        for (j = 2; j < cols  ; j++) {
+            // in each iteration, three values are replaced in the circular queue
+            buffer_pointer = buffer_pointer % 9;
+            buffer[buffer_pointer]   = *(px + j ) ;
+            buffer[buffer_pointer+1] = *(px + j + cols );
+            buffer[buffer_pointer+2] = *(px + j + cols + cols );
+            buffer_pointer+= 3 ;
+            //p_median_f32(buffer, pr, 9, 0, 0);
+            *pr = opt_med9(buffer);
             pr++;
-            px++;
         }
+        px += cols ;
     }
 }
--- src/math/p_median.c
+++ src/math/p_median.c
@@ -1,5 +1,66 @@
 #include <pal.h>
 
+/*
+ *  This Quickselect routine is based on the algorithm described in
+ *  "Numerical recipes in C", Second Edition,
+ *  Cambridge University Press, 1992, Section 8.5, ISBN 0-521-43108-5
+ *  This code by Nicolas Devillard - 1998. Public domain.
+ */
+
+#define ELEM_SWAP(a,b) { register float t=(a);(a)=(b);(b)=t; }
+
+float quick_select(float arr[], int n) 
+{
+    int low, high ;
+    int median;
+    int middle, ll, hh;
+
+    low = 0 ; high = n-1 ; median = (low + high) *.5;
+    for (;;) {
+        if (high <= low) /* One element only */
+            return arr[median] ;
+
+        if (high == low + 1) {  /* Two elements only */
+            if (arr[low] > arr[high])
+                ELEM_SWAP(arr[low], arr[high]) ;
+            return arr[median] ;
+        }
+
+    /* Find median of low, middle and high items; swap into position low */
+    middle = (low + high)*.5;
+    if (arr[middle] > arr[high])    ELEM_SWAP(arr[middle], arr[high]) ;
+    if (arr[low] > arr[high])       ELEM_SWAP(arr[low], arr[high]) ;
+    if (arr[middle] > arr[low])     ELEM_SWAP(arr[middle], arr[low]) ;
+
+    /* Swap low item (now in position middle) into position (low+1) */
+    ELEM_SWAP(arr[middle], arr[low+1]) ;
+
+    /* Nibble from each end towards middle, swapping items when stuck */
+    ll = low + 1;
+    hh = high;
+    for (;;) {
+        do ll++; while (arr[low] > arr[ll]) ;
+        do hh--; while (arr[hh]  > arr[low]) ;
+
+        if (hh < ll)
+        break;
+
+        ELEM_SWAP(arr[ll], arr[hh]) ;
+    }
+
+    /* Swap middle item (in position low) back into correct position */
+    ELEM_SWAP(arr[low], arr[hh]) ;
+
+    /* Re-set active partition */
+    if (hh <= median)
+        low = ll;
+        if (hh >= median)
+        high = hh - 1;
+    }
+}
+
+#undef ELEM_SWAP
+
 /**
  *
  * Calculates the median value of input vector 'a'.
@@ -18,14 +79,9 @@
  *
  */
 
-void p_median_f32(const float *a, float *c, int n, int p, p_team_t team)
+void p_median_f32(float *a, float *c, int n, int p, p_team_t team)
 {
-    float sort[n];
-
-    p_sort_f32(a, sort, n, p, team);
-
-    if(n%2)
-        *c=sort[n>>1];
-    else
-        *c=(sort[n>>1] + sort[(n-1)>>1])*.5;
+    float tmp[n];
+    memcpy(tmp, a, n*sizeof(float) );
+    *c = quick_select(tmp, n);
 }
