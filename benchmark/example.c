1e860ddb87dd3367e06e41700ebc893bb9e7b28c
Gabor Buella <gbuella@gmail.com>
2015-06-08 22:44:57 +0200

    Add prototype for performance measuring
    Simple, portable code
     - only ISO C so far, but time keeping, cycle counting
       is going need platform specific things
     - this way of measuring is not as accurate as instrumentation,
       but more portable, thus gives a better cross-platform picture
    
    The output can be fairly simple, parser friendly
    
    Easy to repeat measurements, with the same, or different input sizes
    Scalability can be tested using increasing input sizes
     - and fancy plots can be created from the outputs
    
    Results easy to compare between commits, given the same platforms for
    running the benchmarks
    
    ( Does not check code size )
    
    ( Not yet integrated with the autoconfig framework, btw I also vote for cmake )
    
    Signed-off-by: Gabor Buella <gbuella@gmail.com>

---
+++ benchmark/example.c
@@ -0,0 +1,28 @@
+
+#include "wrapper.h"
+#include <pal.h>
+
+void bench_p_add(const struct p_benchmark_specification* spec)
+{
+	p_add_f32(spec->mem.input_float_first,
+		spec->mem.input_float_second,
+		spec->mem.output_float,
+		spec->current_size,
+		spec->p,
+		spec->team);
+}
+
+void bench_p_sqrt(const struct p_benchmark_specification* spec)
+{
+	p_sqrt_f32(spec->mem.input_float_first,
+		spec->mem.output_float,
+		spec->current_size,
+		spec->p,
+		spec->team);
+}
+
+const struct p_benchmark_item benchmark_items[] = {
+	{"float addition", bench_p_add},
+	{"float square root", bench_p_sqrt},
+	{NULL, NULL}
+};
