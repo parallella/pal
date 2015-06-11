199f0597f45597e38b1805e3b55266e29054a18c
Tom Anderson <twocs@users.noreply.github.com>
2015-06-12 01:12:18 +0930

    Update description with conversion method
    Float to int is performed by truncating towards zero to the nearest integer.
    Remove a note that I had added earlier as well as an inconsistent punctuation.

--- src/math/p_ftoi.c
+++ src/math/p_ftoi.c
@@ -4,13 +4,13 @@
 
 /**
  *
- * Converts the floating point values in 'a' to signed integer values.
+ * Converts the floating point values in 'a' to signed integer values. Truncates towards zero to the nearest integer.
  *
- * @param a     Pointer to input vector, assume that no element is too close to LONG_MIN or LONG_MAX
+ * @param a     Pointer to input vector
  *
  * @param c     Pointer to output vector
  *
- * @param n     Size of 'a' and 'c' vector.
+ * @param n     Size of 'a' and 'c' vector
  *
  * @param p     Number of processor to use (task parallelism)
  *
