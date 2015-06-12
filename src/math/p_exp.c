91975c9c4553d9fd40650c8102bf4b21a66aff3e
Aleksandar Bukva <aleksandar.bukva@yandex.com>
2015-06-12 11:56:35 +0200

    Fixing build error

--- src/math/p_exp.c
+++ src/math/p_exp.c
@@ -11,10 +11,13 @@
  *
  * @param n     Size of 'a' and 'c' vector.
  *
+ * @param p     Number of processor to use (task parallelism)
+ *
+ * @param team  Team to work with 
+ *
  * @return      None
  *
  */
-<<<<<<< HEAD
 static const float precision = 0.1e-6;
 
 float inverse(float a) //function to calucalte inverse without division operator using Newton Raphson method
@@ -56,10 +59,6 @@ float single_exp(float b)
 	return result;
 }
 void p_exp_f32(const float *a, float *c, int n, int p, p_team_t team)
-=======
-#include <math.h>
-void p_exp_f32(const float *a, float *c, int n)
->>>>>>> upstream/master
 {
 
     int i;
