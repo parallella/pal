959361a0e397ca3fa08e5aab865a6ad65081278b
AlgimantasB <algimantas.bik@gmail.com>
2015-06-12 06:22:40 +0300

    math: p_invcbrt: change division to approximation
    Changed division operator to approximation, borrowed from p_inv.c.
    Also, an idea: maybe it would be okay to add library function for calculating inverse value of single number? That way it could be used everywhere instead of division to reduce code size (unless calling additional function is bigger evil than code repeated in multiple places).

--- src/math/p_invcbrt.c
+++ src/math/p_invcbrt.c
@@ -20,8 +20,24 @@
 #include <math.h>
 void p_invcbrt_f32(const float *a, float *c, int n, int p, p_team_t team)
 {
+    union {
+        float f;
+        uint32_t x;
+    } u;
     int i;
+    float cur;
     for (i = 0; i < n; i++) {
-        *(c + i) = 1.0f / cbrtf(*(a + i));
+        //*(c + i) = 1.0f / cbrtf(*(a + i));
+        u.f = cbrtf(*(a + i));
+        cur = u.f;
+        
+        /* First approximation */
+        u.x = 0x7EEEEBB3 - u.x;
+        /* Refine */
+        u.f = u.f * (2 - u.f * cur);
+        u.f = u.f * (2 - u.f * cur);
+        u.f = u.f * (2 - u.f * cur);
+        *(c + i) = u.f;
+        
     }
 }
