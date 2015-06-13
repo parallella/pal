8133d84d7d0b6a19ea52ca88ba6855c2a4fa4206
Hamid <hamid.ebadi@gmail.com>
2015-06-13 13:51:51 +0200

    conv1d: remove extra element in the result, using uninitialize(unknown) memory addresses in computation

--- src/dsp/p_conv.c
+++ src/dsp/p_conv.c
@@ -1,3 +1,20 @@
+/*
+// FreeMat 
+//--> x = [1, 2, 3, 4];
+//--> h = [5,6,7];
+//--> r = conv(x, h)
+//r =
+//  5 16 34 52 45 28 
+
+// result from pal : 
+   float x[4] = {1,2,3,4};
+   float h[3] = {5,6,7} ;
+   float r[10] = {100,100,100,100,100,100, 100, 100,100, 100};  
+   p_conv_f32( x, h , r ,4 ,3);
+   printf ("%f, %f, %f, %f, %f, %f, %f, %f", r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7]);
+//100.000000, 105.000000, 116.000000, 134.000000, 152.000000, 145.000000, 128.000000, 100.000000
+*/
+
 #include <pal.h>
 
 /**
@@ -27,11 +44,15 @@ void p_conv_f32(const float *x, const float *h, float *r,
 {
     const float *xc = x;
     float *rx = r;
-  	for ( int i = 0; i < nx; i++) {
+    int i,j ;
+        for ( i = 0; i < nx+nh-1; i++) { *(rx++) = 0; } 
+        rx = r ;
+  	for ( i = 0; i < nx; i++) {
         float xv = *xc++;
-        rx++;
-  		for (int j = 0; j < nh; j++) {
-  			*(rx + j) += xv * *(h + j);		
+
+  		for (j = 0; j < nh; j++) {
+  			*(rx + j) += xv * *(h + j);	
   		}
+        rx++;
   	}
 }
