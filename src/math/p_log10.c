2eb2e54b6e36d2a7339bc875997d2cbab4e34024
Nicolas PLANEL <nplanel@redhat.com>
2015-06-15 17:23:03 -0400

    Implement faster base 10 log based on natural log
    Optimized by reused the optimization done on p_ln_f32()
    
    log10(x) = ln(x) / ln(10)

--- src/math/p_log10.c
+++ src/math/p_log10.c
@@ -16,9 +16,10 @@
 #include <math.h>
 void p_log10_f32(const float *a, float *c, int n)
 {
-
+    p_ln_f32(a, c, n);
     int i;
     for (i = 0; i < n; i++) {
-        *(c + i) = log10f(*(a + i));
+        // log10(x) = ln(x) / ln(10)
+        c[i] /= M_LN10;
     }
 }
