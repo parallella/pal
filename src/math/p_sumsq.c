28a3570424e3317896cf6cb70685f3fc3f6595a5
Thomas Böhm <boehm@fortiss.org>
2015-06-17 01:55:45 +0200

    Wrote the sum and sum square functions as a template and added instances for each variable type.

--- src/math/p_sumsq.c
+++ src/math/p_sumsq.c
@@ -2,7 +2,7 @@
 
 /**
  *
- * Calculates the sum of the square of all of the elements vector 'a'.
+ * Calculates the sumsq of the square of all of the elements vector 'a'.
  *
  * @param a     Pointer to input vector
  *
@@ -14,13 +14,25 @@
  *
  */
 
-void p_sumsq_f32(const float *a, float *c, int n)
-{
-    float tmp = 0.0f;
-    int i;
-
-    for (i = 0; i < n; i++) {
-        tmp += *(a + i) * *(a + i);
+#define GEN_FUNC(NAME,TYPE) \
+	/** NAME TYPE */ \
+	void NAME(const TYPE *a, TYPE * restrict c, int n) \
+	{ \
+    	*c = *(a) * *(a);  \
+        for (;--n;) \
+    	    *c += *(a + n) * *(a + n); \
     }
-    *c = tmp;
-}
+
+GEN_FUNC(p_sumsq_f32,float);
+
+GEN_FUNC(p_sumsq_int8,int8_t);
+GEN_FUNC(p_sumsq_uint8,uint8_t);
+
+GEN_FUNC(p_sumsq_int16,int16_t);
+GEN_FUNC(p_sumsq_uint16,uint16_t);
+
+GEN_FUNC(p_sumsq_int32,int32_t);
+GEN_FUNC(p_sumsq_uint32,uint32_t);
+
+GEN_FUNC(p_sumsq_int64,int64_t);
+GEN_FUNC(p_sumsq_uint64,uint64_t);
