7984660b4b8f3dd357b68ebfb57af43f0c7cd0d5
Dominik Taborsky <dominik.taborsky@nic.cz>
2015-06-17 11:30:14 +0200

    math: reimplement cosh along its Taylor series.

--- src/math/p_cosh.c
+++ src/math/p_cosh.c
@@ -18,9 +18,20 @@
 #include <math.h>
 void p_cosh_f32(const float *a, float *c, int n)
 {
+	int i;
+	for (i = 0; i < n; i++) {
+		const float *pa = (a+i);
+		float *pc = (c+i);
+		float val = 0;
+		float theta = M_NORMALIZE_RADIANS(*pa);
+		float theta_sq = theta*theta;
 
-    int i;
-    for (i = 0; i < n; i++) {
-        *(c + i) = coshf(*(a + i));
-    }
+		val = 1.0f + theta_sq*0.01111111f*val;
+		val = 1.0f + theta_sq*0.01785714f*val;
+		val = 1.0f + theta_sq*0.03333333f*val;
+		val = 1.0f + theta_sq*0.08333333f*val;
+		val = 1.0f + theta_sq*0.50000000f*val;
+
+		*pc = val;
+	}
 }
