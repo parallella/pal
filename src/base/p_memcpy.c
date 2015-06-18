4e99ad3ef230e824a68782cbe63f7b6ea1c861fe
Morgan Gangwere <morgan.gangwere@gmail.com>
2015-06-17 17:32:33 -0700

    memcpy: implement
    Implement a basic memcpy, not much to say here.
    
    Signed-off-by: Morgan Gangwere <morgan.gangwere@gmail.com>

--- src/base/p_memcpy.c
+++ src/base/p_memcpy.c
@@ -22,11 +22,43 @@
  *
  */
 #include <stddef.h>
+#ifndef FAIL_FAST
+#include <assert.h>
+#endif
 #include "pal_base.h"
 #include "pal_base_private.h"
 
+static p_mutex_t __global_memset_mutex = 0;
+
+#ifndef __OWN_MEMCPY__
+#define __OWN_MEMCPY__
 ssize_t p_memcpy(void *dst, const void *src, size_t nb, int flags)
 {
-    /*PLACE CODE HERE*/
+#ifdef FAIL_FAST
+    if(src+nb >= dst) {
+        printf("Bad p_memcpy in %s (dst=0x%8x src=0x%8x, nb=%d)",
+            __FILE__,
+            dst,
+            src,
+            nb);
+            
+        abort(); }
+#else
+    assert(src+nb < dst);
+#endif
+    int idx;
+		// Take the mutex if we've not been asked to ignore it.
+    if(!(flags && P_FLAG_ASYNC)) {
+        p_mutex_lock(&__global_memset_mutex);
+    }
+
+    for(idx = 0; idx < nb; idx++) {
+        *((uint8_t*)(dst+idx)) = *((uint8_t*)(src+idx));
+    }
+    // Relinquish the mutex that we took earlier. 
+    if(!(flags && P_FLAG_ASYNC)) {
+      p_mutex_unlock(&__global_memset_mutex);
+    }
     return (0);
 }
+#endif
