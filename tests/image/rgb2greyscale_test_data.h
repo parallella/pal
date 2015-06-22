3959110e9df76cf59125865ae592038222948600
hiovidiu <hiovidiu@gmail.com>
2015-06-23 00:58:42 +0300

    refactored _h

--- tests/image/rgb2greyscale_test_data.h
+++ tests/image/rgb2greyscale_test_data.h
@@ -1,9 +1,8 @@
-// File RGB2G2greyscale_test_data.h
+// File rgb2greyscale_test_data.h
 // 
-// 
-// Created by Matlab function write_data_to_file.m version 0.8a
 
 #define RGB2G(R,G,B) 0.299f*R+0.587f*G+0.114f*B
+#define TRIPLE(V) V, V, V
 
 int in_rows = 2;
 int in_cols = 3;
@@ -18,13 +17,13 @@ float in[7*3] = {
 		};
 
 float out[7*3] = {
-		RGB2G(0.0f, 0.0f, 0.0f), RGB2G(0.0f, 0.0f, 0.0f), RGB2G(0.0f, 0.0f, 0.0f), 
-		RGB2G(0.5f, 0.5f, 0.5f), RGB2G(0.5f, 0.5f, 0.5f), RGB2G(0.5f, 0.5f, 0.5f), 
-		RGB2G(1.0f, 1.0f, 1.0f), RGB2G(1.0f, 1.0f, 1.0f), RGB2G(1.0f, 1.0f, 1.0f), 
-		RGB2G(1.0f, 0.0f, 0.0f), RGB2G(1.0f, 0.0f, 0.0f), RGB2G(1.0f, 0.0f, 0.0f), 
-		RGB2G(0.0f, 1.0f, 0.0f), RGB2G(0.0f, 1.0f, 0.0f), RGB2G(0.0f, 1.0f, 0.0f), 
-		RGB2G(0.0f, 0.0f, 1.0f), RGB2G(0.0f, 0.0f, 1.0f), RGB2G(0.0f, 0.0f, 1.0f),
-		RGB2G(0.333f, 0.333f, 0.333f), RGB2G(0.333f, 0.333f, 0.333f), RGB2G(0.333f, 0.333f, 0.333f)
+		TRIPLE(RGB2G(0.0f, 0.0f, 0.0f)), 
+		TRIPLE(RGB2G(0.5f, 0.5f, 0.5f)), 
+		TRIPLE(RGB2G(1.0f, 1.0f, 1.0f)), 
+		TRIPLE(RGB2G(1.0f, 0.0f, 0.0f)), 
+		TRIPLE(RGB2G(0.0f, 1.0f, 0.0f)), 
+		TRIPLE(RGB2G(0.0f, 0.0f, 1.0f)),
+		TRIPLE(RGB2G(0.333f, 0.333f, 0.333f))
 		};
 int out_size = 2*3*3;
 
