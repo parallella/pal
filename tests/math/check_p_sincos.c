05633d74bb8c6337394467556a39905303b8be27
Ola Jeppsson <ola@adapteva.com>
2015-06-27 13:03:43 +0200

    tests:math: Re-enable p_sincos test
    Port to use utest
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- tests/math/check_p_sincos.c
+++ tests/math/check_p_sincos.c
@@ -2,7 +2,18 @@
    PAL. It has 1 input vector and 2 output vectors. As a result, it needs
    a distinct test infrastructure. */
 
-#include <check.h>
+#include <utest.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <pal.h>
+/* TODO: This relative path include is fragile */
+#include "../../src/base/pal_base_private.h"
+#include <common.h>
+
 #include "simple.h"
 #include "gold/p_sincos_f32.gold.h"
 
@@ -14,7 +25,6 @@ size_t gold_size = ARRAY_SIZE(builtin_gold);
 /* For detecting erroneous overwrites */
 #define OUTPUT_END_MARKER 60189537703610376.0f
 
-__attribute__((weak))
 bool compare(float x, float y)
 {
     float err;
@@ -30,10 +40,11 @@ bool compare(float x, float y)
     return err <= EPSILON_RELMAX;
 }
 
-
-void setup()
+int setup(struct ut_suite *suite)
 {
     size_t i;
+
+    (void) suite;
 
     ai = calloc(gold_size, sizeof(float));
     refCos = calloc(gold_size, sizeof(float));
@@ -50,81 +61,87 @@ void setup()
         ai[i] = gold[i].ai;
     }
 
-    /* Run FUNCTION against gold input here so results are available
-     * for all test cases. */
-    p_sincos_f32(ai, resSin, resCos, gold_size);
+    return 0;
 }
 
-void teardown()
+int teardown(struct ut_suite *suite)
 {
     free(ai);
     free(resCos);
     free(resSin);
     free(refCos);
     free(refSin);
+
+    return 0;
 }
 
-START_TEST(GOLD_TEST)
+int tc_against_gold_e(struct ut_suite *suite, struct ut_tcase *tcase)
+{
+    /* Run FUNCTION against gold input here so results are available
+     * for all test cases. */
+    p_sincos_f32(ai, resSin, resCos, gold_size);
+
+    return 0;
+}
+
+int tc_against_gold_v(struct ut_suite *suite, struct ut_tcase *tcase)
 {
     size_t i;
 
     for (i = 0; i < gold_size; i++) {
-        ck_assert_msg(compare(resSin[i], gold[i].bi),
+        ut_assert_msg(compare(resSin[i], gold[i].bi),
                       "p_sincos(%f): sin: %f != %f",
                       ai[i], resSin[i], gold[i].bi);
-        ck_assert_msg(compare(resCos[i], gold[i].gold),
+        ut_assert_msg(compare(resCos[i], gold[i].gold),
                       "p_sincos(%f): cos: %f != %f",
                       ai[i], resCos[i], gold[i].gold);
     }
-    ck_assert_msg(resSin[i] == OUTPUT_END_MARKER,
+    ut_assert_msg(resSin[i] == OUTPUT_END_MARKER,
                   "Output end marker was overwritten");
-    ck_assert_msg(resCos[i] == OUTPUT_END_MARKER,
+    ut_assert_msg(resCos[i] == OUTPUT_END_MARKER,
                   "Output end marker was overwritten");
-}
-END_TEST
 
-START_TEST(against_ref_function)
+    return 0;
+}
+
+int tc_against_ref_v(struct ut_suite *suite, struct ut_tcase *tcase)
 {
     size_t i;
 
     for (i = 0; i < gold_size; i++) {
-        ck_assert_msg(compare(resSin[i], sinf(gold[i].ai)),
+        ut_assert_msg(compare(resSin[i], sinf(gold[i].ai)),
                       "p_sincos_f32(%f): sin: %f != %f",
                       ai[i], resSin[i], sinf(gold[i].ai));
-        ck_assert_msg(compare(resCos[i], cosf(gold[i].ai)),
+        ut_assert_msg(compare(resCos[i], cosf(gold[i].ai)),
                       "p_sincos_f32(%f): cos: %f != %f",
                       ai[i], resCos[i], cosf(gold[i].ai));
     }
-}
-END_TEST
 
-/* Allow individual tests to add more test cases, e.g. against a reference
- * function */
-__attribute__((weak))
-void add_more_tests(TCase *tcase)
-{
+    return 0;
 }
 
-int main(void)
+DECLARE_UT_TCASE(tc_against_gold, tc_against_gold_e, tc_against_gold_v, NULL);
+DECLARE_UT_TCASE(tc_against_ref, NULL, tc_against_ref_v, NULL);
+
+DECLARE_UT_TCASE_LIST(tcases, &tc_against_gold, &tc_against_ref);
+
+#define FUNCTION_SUITE XCONCAT2(FUNCTION,_suite)
+DECLARE_UT_SUITE(FUNCTION_SUITE, setup, teardown, false, tcases, NULL);
+
+int main(int argc, char *argv[])
 {
-    int num_failures;
-    size_t i;
-    Suite *suite = suite_create("sincos_suite");
-    TCase *tcase = tcase_create("sincos_tcase");
-    SRunner *sr = srunner_create(suite);
+    int ret;
+    char buf[1024] = { 0 };
 
-    tcase_add_unchecked_fixture(tcase, setup, teardown);
+    struct ut_suite *suite;
 
-    tcase_add_test(tcase, GOLD_TEST);
-    tcase_add_test(tcase, against_ref_function);
-    add_more_tests(tcase);
+    suite = &FUNCTION_SUITE;
 
-    suite_add_tcase(suite, tcase);
+    ret = ut_run(suite);
 
-    srunner_run_all(sr, CK_ENV);
-    num_failures = srunner_ntests_failed(sr);
+    ut_report(buf, ARRAY_SIZE(buf), suite, true);
 
-    srunner_free(sr);
+    printf("%s", buf);
 
-    return num_failures ? 1 : 0;
+    return ret;
 }
