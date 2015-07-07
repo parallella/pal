20964514d79e61cede0aad3aa31985a08b62f439
Mansour Moufid <mansourmoufid@gmail.com>
2015-07-07 13:44:28 -0400

    math:acos: Implement the inverse cosine function.
    Signed-off-by: Mansour Moufid <mansourmoufid@gmail.com>

--- src/math/p_acos.c
+++ src/math/p_acos.c
@@ -1,4 +1,7 @@
+#include <math.h>
 #include <pal.h>
+
+static const float pi_2 = (float) M_PI / 2.f;
 
 /**
  *
@@ -15,12 +18,15 @@
  * @return      None
  *
  */
-#include <math.h>
 void p_acos_f32(const float *a, float *c, int n)
 {
 
     int i;
+    float tmp;
+    /* acos x = pi/2 - asin x */
+    p_asin_f32(a, c, n);
     for (i = 0; i < n; i++) {
-        *(c + i) = acosf(*(a + i));
+        tmp = pi_2 - c[i];
+        c[i] = tmp;
     }
 }
