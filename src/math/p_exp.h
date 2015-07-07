d606051841dbd0b5b0795d2e609163896d1d3027
Mansour Moufid <mansourmoufid@gmail.com>
2015-07-07 19:18:01 -0400

    math:exp: Move the core exponential algorithm to a header file.
    Signed-off-by: Mansour Moufid <mansourmoufid@gmail.com>

---
+++ src/math/p_exp.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <math.h>
+
+static const float ln2 = (float) M_LN2;
+
+/*
+ * 0 <= x <= ln 2
+ * e^-x = 1 + a1 * x + a2 * x^2 + ... + a4 * x^4 + e(x)
+ * |e(x)| <= 3 * 10^-5
+ */
+static inline float __p_exp_ln2(const float x)
+{
+    const float a1 = -0.9998684f;
+    const float a2 =  0.4982926f;
+    const float a3 = -0.1595332f;
+    const float a4 =  0.0293641f;
+    float exp_x;
+    exp_x = 1.f +
+        a1 * x +
+        a2 * x * x +
+        a3 * x * x * x +
+        a4 * x * x * x * x;
+    return 1.f / exp_x;
+}
+
+/*
+ * x >= 0
+ * exp x = exp(x' + k ln 2) = (exp x') * 2^k
+ */
+static inline float __p_exp_pos(const float x)
+{
+    long int k, twok;
+    float x_;
+    k = x / ln2;
+    twok = 1U << k;
+    x_ = x - (float) k * ln2;
+    return (float) twok * __p_exp_ln2(x_);
+}
+
+static inline float _p_exp(const float x)
+{
+    if (x >= 0.f)
+        return __p_exp_pos(x);
+    else
+        return 1.f / __p_exp_pos(-x);
+}
