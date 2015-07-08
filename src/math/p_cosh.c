b22bfb98ea94efdb533fa2cf0c39cd650fb90832
Mansour Moufid <mansourmoufid@gmail.com>
2015-07-08 17:23:51 -0400

    math:cosh: Implement the hyperbolic cosine function.
    Signed-off-by: Mansour Moufid <mansourmoufid@gmail.com>

--- src/math/p_cosh.c
+++ src/math/p_cosh.c
@@ -1,5 +1,16 @@
 #include <pal.h>
 
+#include "p_exp.h"
+
+/*
+ * cosh z = (exp z + exp(-z)) / 2
+ */
+static inline float _p_cosh(const float z)
+{
+    float exp_z = _p_exp(z);
+    return 0.5f * (exp_z + 1.f / exp_z);
+}
+
 /**
  *
  * Calculates the hyperbolic cosine of the vector 'a'. Angles are specified
@@ -15,12 +26,11 @@
  *
  */
 
-#include <math.h>
 void p_cosh_f32(const float *a, float *c, int n)
 {
 
     int i;
     for (i = 0; i < n; i++) {
-        *(c + i) = coshf(*(a + i));
+        c[i] = _p_cosh(a[i]);
     }
 }
