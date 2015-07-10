6e11c175b9e071a3ca8bc83fb9b545aec33663ee
James Ross <james.a.ross@gmail.com>
2015-07-10 06:28:43 +0000

    image: p_sobel3x3: new algrthm achieves 76% peak
    The core of the routine achieves 9+ GOPS (over 330 MPixels/s) and
    this is ~76% of theoretical performance using a roofline model based
    on the multiply/add instruction imbalance. The algorithm improves
    upon the earlier code by storing accumulated 3-value columns in
    registers as it processes each row. This is similar to the Prewitt
    and Scharr operators.

--- src/image/p_sobel3x3.c
+++ src/image/p_sobel3x3.c
@@ -1,5 +1,7 @@
 #include <pal.h>
 
+#define FMA(a,b,c) __builtin_fmaf(a,b,c)
+
 static __inline __attribute((__always_inline__)) float my_hypot( float a, float b );
 
 /**
@@ -17,7 +19,7 @@ static __inline __attribute((__always_inline__)) float my_hypot( float a, float 
  *
  * Gradient Direction (theta) = atan2(Gy,Gx)
  *
- * Notes: cols must be a multiple of 2
+ * Notes: cols and rows can be any size
  *
  * @param x     Pointer to input image, a 2D array of size 'rows' x 'cols'
  *
@@ -31,67 +33,110 @@ static __inline __attribute((__always_inline__)) float my_hypot( float a, float 
 
 void p_sobel3x3_f32(const float *x, float *r, int rows, int cols)
 {
+
 	int i, j;
-	float Gx, Gy;
-	float s;
-	float a00, a01, a02, a03, a04;
-	float a10, a11, a12, a13, a14;
-	float a20, a21, a22, a23, a24;
+	float a02, a03, a04, a05;
+	float a12, a13, a14, a15;
+	float a22, a23, a24, a25;
+	float cx2, cx3, cx4, cx5;
+	float cy2, cy3, cy4, cy5;
+	float Gx2, Gx3, Gx4, Gx5;
+	float Gy2, Gy3, Gy4, Gy5;
 
 	const float* px = x;
 	float* pr = r+cols+1;
 
 	for (j = 0; j < (rows - 2); j++) {
-		i = 0;
-		// Unroll 3x is smaller code than 4x and maybe faster. This loop
-		// block may be removed to reduce code size with reduced performance
-		for (; i < (cols - 4); i+=3) {
-			a00 = px[0];
-			a01 = px[1];
+		a04 = px[0];
+		a05 = px[1];
+		a14 = px[cols+0];
+		a15 = px[cols+1];
+		a24 = px[2*cols+0];
+		a25 = px[2*cols+1];
+		cx4 = FMA(2.0f,a14,a04+a24);
+		cx5 = FMA(2.0f,a15,a05+a25);
+		cy4 = a24 - a04;
+		cy5 = a25 - a05;
+		for (i = 0; i < (cols - 5); i+=4) { // unroll 4x
 			a02 = px[2];
+			a12 = px[cols+2];
+			a22 = px[2*cols+2];
 			a03 = px[3];
-			a04 = px[4];
-			a10 = px[cols];
-			a11 = px[cols+1];
-			a12 = px[cols+2];
 			a13 = px[cols+3];
-			a14 = px[cols+4];
-			a20 = px[2*cols];
-			a21 = px[2*cols+1];
-			a22 = px[2*cols+2];
 			a23 = px[2*cols+3];
+			a04 = px[4];
+			a14 = px[cols+4];
 			a24 = px[2*cols+4];
-			s = a22 - a00;
-			Gx = s + __builtin_fmaf(-2.0f,a10,-a20) + __builtin_fmaf(2.0f,a12,a02);
-			Gy = s + __builtin_fmaf(-2.0f,a01,-a02) + __builtin_fmaf(2.0f,a21,a20);
-			*(pr++) = my_hypot(Gx, Gy) * M_DIV8;
-			s = a23 - a01;
-			Gx = s + __builtin_fmaf(-2.0f,a11,-a21) + __builtin_fmaf(2.0f,a13,a03);
-			Gy = s + __builtin_fmaf(-2.0f,a02,-a03) + __builtin_fmaf(2.0f,a22,a21);
-			*(pr++) = my_hypot(Gx, Gy) * M_DIV8;
-			s = a24 - a02;
-			Gx = s + __builtin_fmaf(-2.0f,a12,-a22) + __builtin_fmaf(2.0f,a14,a04);
-			Gy = s + __builtin_fmaf(-2.0f,a03,-a04) + __builtin_fmaf(2.0f,a23,a22);
-			*(pr++) = my_hypot(Gx, Gy) * M_DIV8;
-			px += 3;
+			a05 = px[5];
+			a15 = px[cols+5];
+			a25 = px[2*cols+5];
+			cx2 = FMA(2.0f,a12,a02+a22);
+			cy2 = a22 - a02;
+			Gx2 = cx2 - cx4;
+			Gy2 = FMA(2.0f,cy5,cy4+cy2);
+			cx3 = FMA(2.0f,a13,a03+a23);
+			cy3 = a23 - a03;
+			Gx3 = cx3 - cx5;
+			Gy3 = FMA(2.0f,cy2,cy5+cy3);
+			cx4 = FMA(2.0f,a14,a04+a24);
+			cy4 = a24 - a04;
+			Gx4 = cx4 - cx2;
+			Gy4 = FMA(2.0f,cy3,cy2+cy4);
+			cx5 = FMA(2.0f,a15,a05+a25);
+			cy5 = a25 - a05;
+			Gx5 = cx5 - cx3;
+			Gy5 = FMA(2.0f,cy4,cy3+cy5);
+			*(pr++) = my_hypot(Gx2, Gy2) * M_DIV8;
+			*(pr++) = my_hypot(Gx3, Gy3) * M_DIV8;
+			*(pr++) = my_hypot(Gx4, Gy4) * M_DIV8;
+			*(pr++) = my_hypot(Gx5, Gy5) * M_DIV8;
+			px += 4;
 		}
-		// catching remainder (or full loop if loop above is removed)
-		for (; i < (cols - 2); i++) {
-			a00 = px[0];
-			a01 = px[1];
-			a02 = px[2];
-			a10 = px[cols];
-			a11 = px[cols+1];
-			a12 = px[cols+2];
-			a20 = px[2*cols];
-			a21 = px[2*cols+1];
-			a22 = px[2*cols+2];
-			s = a22 - a00;
-			Gx = s + __builtin_fmaf(-2.0f,a10,-a20) + __builtin_fmaf(2.0f,a12,a02);
-			Gy = s + __builtin_fmaf(-2.0f,a01,-a02) + __builtin_fmaf(2.0f,a21,a20);
-			*(pr++) = my_hypot(Gx, Gy) * M_DIV8;
-			px++;
+		switch(cols-i) { // catching remainder
+			case 5:
+				a05 = px[2];
+				a15 = px[cols+2];
+				a25 = px[2*cols+2];
+				cx3 = cx4;
+				cx4 = cx5;
+				cy3 = cy4;
+				cy4 = cy5;
+				cx5 = FMA(2.0f,a15,a05+a25);
+				cy5 = a25 - a05;
+				Gx5 = cx5 - cx3;
+				Gy5 = FMA(2.0f,cy4,cy3+cy5);
+				*(pr++) = my_hypot(Gx5, Gy5) * M_DIV8;
+				px++;
+			case 4:
+				a05 = px[2];
+				a15 = px[cols+2];
+				a25 = px[2*cols+2];
+				cx3 = cx4;
+				cx4 = cx5;
+				cy3 = cy4;
+				cy4 = cy5;
+				cx5 = FMA(2.0f,a15,a05+a25);
+				cy5 = a25 - a05;
+				Gx5 = cx5 - cx3;
+				Gy5 = FMA(2.0f,cy4,cy3+cy5);
+				*(pr++) = my_hypot(Gx5, Gy5) * M_DIV8;
+				px++;
+			case 3:
+				a05 = px[2];
+				a15 = px[cols+2];
+				a25 = px[2*cols+2];
+				cx3 = cx4;
+				cx4 = cx5;
+				cy3 = cy4;
+				cy4 = cy5;
+				cx5 = FMA(2.0f,a15,a05+a25);
+				cy5 = a25 - a05;
+				Gx5 = cx5 - cx3;
+				Gy5 = FMA(2.0f,cy4,cy3+cy5);
+				*(pr++) = my_hypot(Gx5, Gy5) * M_DIV8;
+				px++;
 		}
+
 		pr += 2;
 		px += 2;
 	}
@@ -120,12 +165,12 @@ void p_sobel3x3_f32(const float *x, float *r, int rows, int cols)
 
 static __inline __attribute((__always_inline__)) float my_hypot( float a, float b )
 {
-	float s2 = __builtin_fmaf(a,a,b * b);
+	float s2 = FMA(a,a,b * b);
 	float x = s2 * -0.5f;
 	long i  = * ( long * ) &s2;
 	i  = 0x5f375a86 - ( i >> 1 );
 	float y  = * ( float * ) &i;
-	y = y * __builtin_fmaf(x, y*y, 1.5f); // 1st Newton iteration
-	y = y * __builtin_fmaf(x, y*y, 1.5f); // 2nd iteration, this can be removed
+	y = y * FMA(x, y*y, 1.5f); // 1st Newton iteration
+	y = y * FMA(x, y*y, 1.5f); // 2nd iteration, this can be removed
 	return s2 * y;
 }
