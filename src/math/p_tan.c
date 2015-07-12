03516d90d41770c59c82b49b4888eae2dbf2c2b9
Mansour Moufid <mansourmoufid@gmail.com>
2015-07-12 10:05:39 -0400

    math: tan: Use the unary mapping function.
    Signed-off-by: Mansour Moufid <mansourmoufid@gmail.com>

--- src/math/p_tan.c
+++ src/math/p_tan.c
@@ -85,8 +85,5 @@ static inline float _p_tan(const float x)
  */
 void p_tan_f32(const float *a, float *c, int n)
 {
-    int i;
-    for (i = 0; i < n; i++) {
-        c[i] = _p_tan(a[i]);
-    }
+    p_map_unary(&_p_tan, a, c, n);
 }
