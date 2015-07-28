2a286cc923816ec4b40267cd0b7557b22b671ca4
Haneef Mubarak <haneef503@gmail.com>
2015-07-28 06:44:46 -0700

    sync: p_atomic_compswap: implement atomic CAS
    Signed-off-by: Haneef Mubarak <haneef503@gmail.com>

--- include/pal_base.h
+++ include/pal_base.h
@@ -240,7 +240,14 @@ int32_t p_atomic_swap_i32(int32_t *atom, int32_t n);
 int64_t p_atomic_swap_i64(int64_t *atom, int64_t n);
 
 /*atomic compare and swap*/
-int p_atomic_compswap_u32(p_atom_t atom, uint32_t *input, uint32_t expected);
+uint8_t p_atomic_compswap_u8(uint8_t *atom, uint8_t oldval, uint8_t newval);
+uint16_t p_atomic_compswap_u16(uint16_t *atom, uint16_t oldval, uint16_t newval);
+uint32_t p_atomic_compswap_u32(uint32_t *atom, uint32_t oldval, uint32_t newval);
+uint64_t p_atomic_compswap_u64(uint64_t *atom, uint64_t oldval, uint64_t newval);
+int8_t p_atomic_compswap_i8(int8_t *atom, int8_t oldval, int8_t newval);
+int16_t p_atomic_compswap_i16(int16_t *atom, int16_t oldval, int16_t newval);
+int32_t p_atomic_compswap_i32(int32_t *atom, int32_t oldval, int32_t newval);
+int64_t p_atomic_compswap_i64(int64_t *atom, int64_t oldval, int64_t newval);
 
 /** @todo Add description */
 int p_getaddr(p_mem_t mem);
