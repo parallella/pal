abe9443050cd52d53aa908131f6664d5d951f9b4
Ola Jeppsson <ola@adapteva.com>
2015-08-13 22:07:45 +0000

    tests:image:check_p_rgb2greyscale_f32: Add support for Epiphany
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- tests/image/check_p_rgb2greyscale_f32.c
+++ tests/image/check_p_rgb2greyscale_f32.c
@@ -2,22 +2,25 @@
 Host program for testing of DSP functions in PAL DSP lib
 
 This program is prepared to call one or several Epiphany cores to run the test.
-
-Currently, the test is only using Arm
-
 */
 
-
+#include <utest.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <math.h>
 #include <pal.h>
+#include <common.h>
+
 #include "rgb2greyscale_test_data.h"
 
 #define MAX_REL_DIFF  0.0001
 #define OK 1
 #define NOK 0
+
+float test_out[ARRAY_SIZE(out)];
 
 int check_data(float tst, float ref, float max_diff)
 {
@@ -41,6 +44,88 @@ int check_data(float tst, float ref, float max_diff)
   }
 }
 
+int tc_against_gold_e(struct ut_suite *suite, struct ut_tcase *tcase)
+{
+    p_rgb2grayscale_f32(in, test_out, in_rows, in_cols);
+
+    return 0;
+}
+
+int tc_against_gold_v(struct ut_suite *suite, struct ut_tcase *tcase)
+{
+    size_t i;
+
+    for ( i = 0; i < out_size; i++ ) {
+        ut_assert_msg((check_data(test_out[i], out[i], MAX_REL_DIFF) == OK),
+                      "p_rgb2greyscale_f32(): Large diff for index: %d, ref: %f, test: %f, rel: %f\n",
+                      i, out[i], test_out[i], out[i]/test_out[i]);
+    }
+
+    return 0;
+}
+
+int setup(struct ut_suite *suite)
+{
+    (void) suite;
+
+    return 0;
+}
+
+int teardown(struct ut_suite *suite)
+{
+    (void) suite;
+
+    return 0;
+}
+
+DECLARE_UT_TCASE(tc_against_gold, tc_against_gold_e, tc_against_gold_v, NULL);
+DECLARE_UT_TCASE_LIST(tcases, &tc_against_gold);
+DECLARE_UT_SUITE(p_rgb2greyscale_f32_suite, setup, teardown, false, tcases, NULL);
+
+#ifdef __epiphany__
+struct status {
+    uint32_t done;
+    uint32_t _pad1;
+    uint32_t returncode;
+    uint32_t _pad2;
+} __attribute__((packed));
+
+volatile struct status *epiphany_status = (struct status *) 0x8f200000;
+volatile char *epiphany_results = (char *) 0x8f300000;
+#endif
+
+int main(int argc, char *argv[])
+{
+    int ret;
+    char buf[1024] = { 0 };
+
+    struct ut_suite *suite = &p_rgb2greyscale_f32_suite;
+
+    ret = ut_run(suite);
+
+    ut_report(buf, ARRAY_SIZE(buf), suite, true);
+#ifdef __epiphany__
+    memcpy((void *) epiphany_results, buf, sizeof(buf));
+    epiphany_status->returncode = ret;
+    epiphany_status->done = 1;
+#else
+    printf("%s", buf);
+#endif
+
+#if 0
+    p = test_out;
+    for ( i = 0; i < ROWS - 2; i++ ) {
+        for ( j = 0; j < COLS - 2; j++ ) {
+            printf("%f\t", *p++);
+        }
+        printf("\n");
+    }
+#endif
+
+    return ret;
+}
+
+#if 0
 int main(int argc, char *argv[])
 {
     // Stack variables
@@ -67,4 +152,4 @@ int main(int argc, char *argv[])
 
     return testFail;
 }
-
+#endif
