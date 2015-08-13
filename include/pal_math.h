fd1951c9a8ce3133483a4bdc7cf7a5495375a317
Ola Jeppsson <ola@adapteva.com>
2015-08-13 17:06:12 +0000

    include: Fix compiler warning in pal_math.h for Epiphany
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- include/pal_math.h
+++ include/pal_math.h
@@ -5,7 +5,9 @@
 #define M_E        2.7182818284590452354     /* e */
 #define M_LOG2E    1.4426950408889634074     /* log 2e */
 #define M_LOG10E   0.43429448190325182765    /* log 10e */
+#ifndef M_LN2 /* Avoid warning, newlib defines this as _M_LN2 */
 #define M_LN2      0.69314718055994530942    /* log e2 */
+#endif
 #define M_LN10     2.30258509299404568402    /* log e10 */
 #define M_PI       3.14159265358979323846    /* pi */
 #define M_PI_2     1.57079632679489661923    /* pi/2 */
