b93cf80ba46ba6aeb8130e4ebe71f27d8b90b0df
Ola Jeppsson <ola@adapteva.com>
2015-08-14 10:01:12 +0000

    tests:math: Work around intential coverity positive in rangen
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- tests/math/rangen.c
+++ tests/math/rangen.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     srand((unsigned int)time(NULL));
     float x, y, z;
     for (i = 0; i < 100; i++) {
-        x = 1.0f - (float)rand() / (float)(RAND_MAX / 2);
-        y = 1.0f - (float)rand() / (float)(RAND_MAX / 2);
+        x = 1.0f - ((float) rand()) / ((float) (RAND_MAX) / 2.0f);
+        y = 1.0f - ((float) rand()) / ((float) (RAND_MAX) / 2.0f);
         printf("%f,%f,0,0\n", x, y);
     }
     return 0;
