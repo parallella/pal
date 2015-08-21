ffd01e737d5025734e3058d712a5496260a497e2
Ola Jeppsson <ola@adapteva.com>
2015-08-21 16:48:23 +0000

    examples:image: Working filters2 prototype
    Host side code is almost clean.
    Device needs more work.
    
    Hints:
    ```
    cd ~/pal
    ./bootstrap
    
    mkdir -p build.{epiphany,arm}
    
    cd build.epiphany
    
    ../configure CFLAGS="-O2 -mno-soft-cmpsf -mfp-mode=truncate \
    -ffp-contract=fast -ffast-math"
    
    make -j2
    
    cd ../build.arm
    
    ../configure
    
    EPIPHANY_CFLAGS="-O2 -mno-soft-cmpsf -mfp-mode=truncate \
    -ffp-contract=fast -ffast-math -std=c99" \
    EPIPHANY_LDFLAGS="-Wl,-T/opt/adapteva/esdk/bsps/current/internal.ldf" \
    EPIPHANY_CPPFLAGS=-I${HOME}/pal/include \
    EPIPHANY_LDADD=~/pal/build.epiphany/src/libpal.la make -j2
    ```
    
    Work in progress...
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- examples/image/filters2-dev.c
+++ examples/image/filters2-dev.c
@@ -1,5 +1,84 @@
 /* filters 2 device file */
+#include <pal.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#ifndef __epiphany__
+#error "Atm. only Epiphany is supported"
+#endif
+
+#define CLAMP(x, low, high) (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
+
+void float_to_ubyte(unsigned char *dest, float *src, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        dest[i] = (unsigned char)CLAMP(src[i] * 255.0f, 0.0f, 255.0f);
+}
+
+void gauss3x3(float *x, float *r, int rows, int cols)
+{
+    size_t size = rows * cols;
+    float my_x[size];
+    float my_r[size];
+
+    memcpy(my_x, x, size * sizeof(float));
+
+    p_gauss3x3_f32(my_x, my_r, rows, cols);
+
+    memcpy(r, my_r, size * sizeof(float));
+}
+
+#if 1
+// HACK: PAL will automagically do the function call for us
+// Everything below will unnecessary
+struct epiphany_ctrl_mem {
+    uint32_t status[16]; // status field for team
+    uint32_t argsoffset;
+} __attribute__((packed));
+
+struct epiphany_args_header {
+    uint32_t nargs;
+    uint32_t __pad1;
+    uint32_t size[P_RUN_MAX_ARGS];
+} __attribute__((packed));
+
 int main()
 {
+    float *out = (float *) 0xdeadbeef;
+    float *img;
+    int h, w;
+
+#define CTRL_MEM_SIZE 4096
+#define CTRL_MEM_EADDR (0x90000000-CTRL_MEM_SIZE)
+#define ARGS_MEM_END_EADDR CTRL_MEM_EADDR
+    volatile struct epiphany_ctrl_mem *ctrl =
+        (struct epiphany_ctrl_mem *) CTRL_MEM_EADDR;
+
+    uintptr_t argsoffset = ctrl->argsoffset;
+
+    struct epiphany_args_header *header = (struct epiphany_args_header *)
+        (((uintptr_t) ARGS_MEM_END_EADDR) - argsoffset);
+
+    uint8_t *arg0 = (void *) &header[1];
+
+    uint8_t *argp = arg0;
+
+    memcpy(&h, argp, sizeof(h));
+    argp += sizeof(h);
+    memcpy(&w, argp, sizeof(w));
+    argp += sizeof(w);
+
+    size_t size = w * h;
+    img = (float *) argp;
+    argp += size * sizeof(float);
+    memcpy(&out, argp, sizeof(out));
+    argp += sizeof(out);
+
+    gauss3x3(img, out, h, w);
+
     return 0;
 }
+#endif
--- examples/image/filters2-host.c
+++ examples/image/filters2-host.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <pal_image.h>
+
+#include <pal.h>
+#include <common.h>
 
 /* stb image */
 #define STB_IMAGE_IMPLEMENTATION
@@ -10,20 +12,10 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
+#define BINARY ("filters2-dev-" XSTRING(DEVICE))
+
 /* macros */
 #define CLAMP(x, low, high) (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
-
-void set_working_dir(char *argv[])
-{
-    char *wdir = strdup(argv[0]);
-    char *s = strrchr(wdir,'/');
-    *s = '\0';
-    if (chdir(wdir)) {
-        perror(argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    free(wdir);
-}
 
 void float_to_ubyte(unsigned char *dest, float *src, int size)
 {
@@ -32,7 +24,6 @@ void float_to_ubyte(unsigned char *dest, float *src, int size)
         dest[i] = (unsigned char)CLAMP(src[i] * 255, 0, 255);
 }
 
-
 int main(int argc, char *argv[])
 {
     float *data;
@@ -40,26 +31,27 @@ int main(int argc, char *argv[])
     unsigned char *data_ub; /* ubyte data */
     int w, h, n; /* with, height, n-components*/
     int i, size;
-    
-    set_working_dir(argv);
-    
+
+    int err;
+
+
     /* read and convert image */
     {
         float uctof = 1.0f / 255.0f;
-        
+
         /* read */
-        data_ub = stbi_load(ABS_TOP_SRCDIR "/examples/image/dataset/lena.tga",
+        data_ub = stbi_load(ABS_TOP_SRCDIR "/examples/image/dataset/lena-small.tga",
                             &w, &h, &n, 0);
         if (! data_ub) {
             printf("unable to read image\n");
             return EXIT_FAILURE;
         }
-        
+
         /* allocate memory */
         size = w * h;
         data = malloc(size * sizeof(float));
         dest = malloc(size * sizeof(float));
-        
+
         /* convert to float */
         if (n == 1) /* already greyscale */
             for (i = 0; i < size; i++)
@@ -68,60 +60,45 @@ int main(int argc, char *argv[])
             for (i = 0; i < size; i++)
                 data[i] = (data_ub[i*n]*0.2f + data_ub[i*n + 1]*0.7f + data_ub[i*n + 2]*0.1f) * uctof;
     }
-    
-    /* gaussian */
-    {
-        p_gauss3x3_f32(data, dest, h, w);
-        float_to_ubyte(data_ub, dest, size);
-        stbi_write_tga("../dataset/lena_gaussian.tga", w, h, 1, data_ub);
-    }
-    
-    /* harris */
-    {
-        float *tmp = malloc(size * 3 * sizeof(float));
-        p_harris3x3_f32(data, dest, tmp, h, w);
-	free(tmp);
-        float_to_ubyte(data_ub, dest, size);
-        stbi_write_tga("../dataset/lena_harris.tga", w-4, h-4, 1, data_ub);
-    }
-    
-    /* sobel */
-    {
-        p_sobel3x3_f32(data, dest, h, w);
-        float_to_ubyte(data_ub, dest, size);
-        stbi_write_tga("../dataset/lena_sobel.tga", w, h, 1, data_ub);
-    }
 
-    /* scharr */
-    {
-        p_scharr3x3_f32(data, dest, h, w);
-        float_to_ubyte(data_ub, dest, size);
-        stbi_write_tga("../dataset/lena_scharr.tga", w, h, 1, data_ub);
-    }
+    // initialize device and team
+    p_dev_t dev = p_init(P_DEV_EPIPHANY, 0);
 
-    /* prewitt */
-    {
-        p_prewitt3x3_f32(data, dest, h, w);
-        float_to_ubyte(data_ub, dest, size);
-        stbi_write_tga("../dataset/lena_prewitt.tga", w, h, 1, data_ub);
-    }
-    
-    /* box */
-    {
-        p_box3x3_f32(data, dest, h, w);
-        float_to_ubyte(data_ub, dest, size);
-        stbi_write_tga("../dataset/lena_box.tga", w-2, h-2, 1, data_ub);
-    }
-    
-    /* median */
-    {
-        p_median3x3_f32(data, dest, h, w);
-        float_to_ubyte(data_ub, dest, size);
-        stbi_write_tga("../dataset/lena_median.tga", w-2, h-2, 1, data_ub);
-    }
-    
-    free(dest);
-    free(data);
+    // load a program from file system
+    p_prog_t prog = p_load(dev, BINARY, 0);
+
+    // Open team. FIXME: must be 16 for Epiphany
+    p_team_t team = p_open(dev, 0, 16); // create a team
+
+    // Copy data to device. TODO: We need a API call for this
+    float *out = (float *) 0x8f900000;
+    memset(out, 0, size * sizeof(float));
+
+    // Construct program arguments
+    // TODO: Uncessary copy even if data pointed to is already in
+    // memory device can access.
+    p_arg_t args[] = {
+        &h, sizeof(h),
+        &w, sizeof(w),
+        data, (size * sizeof(float)),
+        &out, sizeof(out),
+    };
+
+    // Run program. TODO: Function name argument is no-op.
+    err = p_run(prog, "gauss3x3", team, 0, 1, ARRAY_SIZE(args), args, 0);
+
+    // Read back result. TODO: Use PAL API.
+    memcpy(data, (void *) out, size * sizeof(float));
+
+    // TODO: Perform on device
+    float_to_ubyte(data_ub, (float *) data, size);
+
+    stbi_write_tga("dataset/lena_gaussian.tga", w, h, 1, data_ub);
+
     stbi_image_free(data_ub);
-    return EXIT_SUCCESS;
+
+    // TODO: These are no-ops atm.
+    p_close(team);
+
+    p_finalize(dev);
 }
