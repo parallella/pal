4821686e8fbca2972cb57f67d1bffec5ea039307
Chris Bruner <cbruner@quadro.net>
2015-08-31 11:35:47 -0400

    function did not agree with prototype. Appears to be a stubb so removing warnings and making it match prototype.

--- src/base/p_broadcast.c
+++ src/base/p_broadcast.c
@@ -1,12 +1,18 @@
 #include <pal.h>
-
+#include <stdio.h>
 #include <stddef.h>
 #include "pal_base.h"
 #include "pal_base_private.h"
 
-ssize_t p_broadcast(p_mem_t *mlist[], void *src, size_t nsrc, size_t ndst,
-                    int flags)
+ssize_t p_scatter(p_mem_t *mlist[], int mcount, void *suf, size_t scount,
+                  int disp[], int flags)
 {
+    UNUSED(mlist);
+    UNUSED(mcount);
+    UNUSED(suf);
+    UNUSED(scount);
+    UNUSED(disp);
+    UNUSED(flags);
     printf("Running p_broadcast()\n");
     return (0);
 }
