09ab666e75531456fa87e6a415bfcc284138e460
Ola Jeppsson <ola@adapteva.com>
2016-06-20 22:44:52 +0200

    math: p_exp.h: Use generic types
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- src/math/p_exp.h
+++ src/math/p_exp.h
@@ -1,46 +1,46 @@
 #pragma once
 #include <pal.h>
 
-static const float ln2 = (float) M_LN2;
+static const PTYPE ln2 = PCONST(M_LN2);
 
 /*
  * 0 <= x <= ln 2
  * e^-x = 1 + a1 * x + a2 * x^2 + ... + a4 * x^4 + e(x)
  * |e(x)| <= 3 * 10^-5
  */
-static inline float __p_exp_ln2(const float x)
+static inline PTYPE __p_exp_ln2(const float x)
 {
-    const float a1 = -0.9998684f;
-    const float a2 =  0.4982926f;
-    const float a3 = -0.1595332f;
-    const float a4 =  0.0293641f;
-    float exp_x;
-    exp_x = 1.f +
+    const PTYPE a1 = PCONST(-0.9998684);
+    const PTYPE a2 = PCONST(0.4982926);
+    const PTYPE a3 = PCONST(-0.1595332);
+    const PTYPE a4 = PCONST(0.0293641);
+    PTYPE exp_x;
+    exp_x = PCONST(1.0) +
         a1 * x +
         a2 * x * x +
         a3 * x * x * x +
         a4 * x * x * x * x;
-    return 1.f / exp_x;
+    return PCONST(1.0) / exp_x;
 }
 
 /*
  * x >= 0
  * exp x = exp(x' + k ln 2) = (exp x') * 2^k
  */
-static inline float __p_exp_pos(const float x)
+static inline PTYPE __p_exp_pos(const PTYPE x)
 {
     long int k, twok;
-    float x_;
+    PTYPE x_;
     k = x / ln2;
-    twok = 1U << k;
-    x_ = x - (float) k * ln2;
-    return (float) twok * __p_exp_ln2(x_);
+    twok = 1ULL << k;
+    x_ = x - (PTYPE) k * ln2;
+    return (PTYPE) twok * __p_exp_ln2(x_);
 }
 
-static inline float _p_exp(const float x)
+static inline PTYPE _p_exp(const PTYPE x)
 {
-    if (x >= 0.f)
+    if (x >= PCONST(0.0))
         return __p_exp_pos(x);
     else
-        return 1.f / __p_exp_pos(-x);
+        return PCONST(1.0) / __p_exp_pos(-x);
 }
