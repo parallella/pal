54dd8e5ff8c9ab67ad7074109bce309d4f032510
Ola Jeppsson <ola@adapteva.com>
2016-07-08 15:18:34 +0200

    tests: runtest.epiphany: Switch from e-hal API to PAL API
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- tests/runtest.epiphany.c
+++ tests/runtest.epiphany.c
@@ -1,15 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdbool.h>
 #include <stdint.h>
-#include <inttypes.h>
 
-#include <e-hal.h>
-
-static const uint8_t empty[128] = { 0 };
+#include <pal.h>
 
 /* https://www.gnu.org/software/automake/manual/html_node/Scripts_002dbased-Testsuites.html */
 #define HARD_ERROR 99
@@ -21,79 +15,41 @@ struct status {
     uint32_t _pad2;
 } __attribute__((packed));
 
-int main(int argc, char **argv, char **envp)
+void usage(char **argv)
 {
-    e_platform_t platform;
-    e_epiphany_t dev;
-    e_mem_t   result_handle;
-    e_mem_t   status_handle;
-    struct status status;
+    fprintf(stderr, "Usage: %s TEST\n", argv[0]);
+    exit(HARD_ERROR);
+}
+
+int main(int argc, char **argv)
+{
+    int returncode, err;
     char *results;
-    uint32_t i;
+    struct status *status;
+    p_dev_t dev;
+    p_prog_t prog;
+    p_team_t team;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s PROGRAM\n", argv[0]);
-        return 1;
-    }
+    if (argc != 2)
+        usage(argv);
 
-    e_init(NULL);
-    e_reset_system();
-    e_get_platform_info(&platform);
+    dev = p_init(P_DEV_EPIPHANY, 0);
+    prog = p_load(dev, argv[1], 0);
+    team = p_open(dev, 0, 16); // TODO: Must be 16 for Epiphany
 
-    if (e_open(&dev, 0, 0, platform.rows, platform.cols)) {
-        fprintf(stderr, "ERROR: Cannot open device\n");
-        exit(HARD_ERROR);
-    }
-    if (e_alloc(&status_handle, 0x01200000, 4096)) {
-        fprintf(stderr, "ERROR: Failed allocating buffer\n");
-        exit(HARD_ERROR);
-    }
-    if (0 >= e_write(&status_handle, 0, 0, 0, empty, sizeof(empty))) {
-        fprintf(stderr, "ERROR: Writing to ERAM failed\n");
-        exit(HARD_ERROR);
-    }
-    if (e_alloc(&result_handle, 0x01300000, 1024 * 1024)) {
-        fprintf(stderr, "ERROR: Failed allocating buffer\n");
-        exit(HARD_ERROR);
-    }
-    if (0 >= e_write(&result_handle, 0, 0, 0, empty, sizeof(empty))) {
-        fprintf(stderr, "ERROR: Writing to ERAM failed\n");
-        exit(HARD_ERROR);
-    }
-    if (e_load_group(argv[1], &dev, 0, 0, 1, 1, true)) {
-        fprintf(stderr, "ERROR: Cannot load binary\n");
-        exit(HARD_ERROR);
-    }
+    status = p_map(dev, 0x8f200000, sizeof(*status));
+    results = p_map(dev, 0x8f300000, 1024);
 
-    while (true) {
-        if (0 >= e_read(&status_handle, 0, 0, 0x00000000, &status,
-            sizeof(status))) {
-            exit(HARD_ERROR);
-        }
+    memset(status, 0, sizeof(*status));
+    memset(results, 0, 1024);
 
-        if (status.done)
-            break;
-
-        usleep(10000);
-    }
-
-    results = calloc(1024 * 1024, 1);
-    if (!results) {
-        fprintf(stderr, "ERROR: No memory\n");
-        exit(HARD_ERROR);
-    }
-
-    if (0 >= e_read(&result_handle, 0, 0, 0, results, 1024)) {
-        fprintf(stderr, "ERROR: Failed reading result\n");
-        exit(HARD_ERROR);
-    }
-
-    e_close(&dev);
-    e_finalize();
+    err = p_run(prog, "main", team, 0, 1, 0, NULL, 0);
+    returncode = err ? HARD_ERROR : status->returncode;
 
     printf("%s\n", results);
 
-    free(results);
+    p_close(team);
+    p_finalize(dev);
 
-    return status.returncode;
+    return returncode;
 }
