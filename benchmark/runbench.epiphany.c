9027efc8db369b8dafde148f00cbe6b870abbd56
Ola Jeppsson <ola@adapteva.com>
2016-07-21 23:15:16 +0200

    benchmark/runbench.epiphany: Update to match API
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- benchmark/runbench.epiphany.c
+++ benchmark/runbench.epiphany.c
@@ -33,8 +33,10 @@ void usage(char **argv)
 int main(int argc, char **argv)
 {
     int returncode, err, i;
-    struct status *status;
+    struct status status = { 0 };
     struct result *results;
+    uint8_t clear[1024 * 1024] = { 0 };
+    p_mem_t status_mem, results_mem;
     p_dev_t dev;
     p_prog_t prog;
     p_team_t team;
@@ -46,19 +48,24 @@ int main(int argc, char **argv)
     prog = p_load(dev, argv[1], 0);
     team = p_open(dev, 0, 16); // TODO: Must be 16 for Epiphany
 
-    status = p_map(dev, 0x8f200000, sizeof(*status));
-    results = p_map(dev, 0x8f300000, 1024 * 1024);
+    status_mem = p_map(dev, 0x8f200000, sizeof(status));
+    results_mem = p_map(dev, 0x8f300000, 1024 * 1024);
 
-    memset(status, 0, sizeof(*status));
-    memset(results, 0, 1024 * 1024);
+    /* Clear */
+    p_write(&status_mem, &status, 0, sizeof(status), 0);
+    p_write(&results_mem, clear, 0, sizeof(clear), 0);
 
     err = p_run(prog, "main", team, 0, 1, 0, NULL, 0);
 
+    /* Read back */
+    p_read(&status_mem, &status, 0, sizeof(status), 0);
+    results = alloca(sizeof(*results) * status.nbench);
+    p_read(&results_mem, results, 0, sizeof(*results) * status.nbench, 0);
+
     printf(";name, size, duration (ns)\n");
-    for (i = 0; i < status->nbench; i++)
+    for (i = 0; i < status.nbench; i++)
         printf("%s, %" PRIu64 ", %" PRIu64 "\n",
                results[i].name, results[i].size, results[i].ns);
-
 
     p_close(team);
     p_finalize(dev);
