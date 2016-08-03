16f026a9f123188685f8bf5678ce19ec2fc7a248
Ola Jeppsson <ola@adapteva.com>
2016-08-03 16:25:42 +0200

    p_rel_coords_to_rank: Support negative offsets
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- src/base/p_rel_coords_to_rank.c
+++ src/base/p_rel_coords_to_rank.c
@@ -8,9 +8,9 @@
  * @param team     The team.
  * @param coords   Coordinates. Relative to team base.
  *                 Must have same topology as the team.
- * @param flags  flags.
+ * @param flags    flags.
  *
- * @return      Return zero on success. Negative on error.
+ * @return         Return rank on success. Negative value indicates  error.
  *
  */
 int p_rel_coords_to_rank(p_team_t team, int rank, const p_coords_t *coords,
@@ -22,24 +22,39 @@ int p_rel_coords_to_rank(p_team_t team, int rank, const p_coords_t *coords,
     if (p_error(pteam))
         return p_error(pteam);
 
-    abs_coords = pteam->rank;
+    if (p_rank_to_coords(pteam, rank, &abs_coords, 0))
+        return -EINVAL;
 
-    /* TODO: Overflow + negative relative coords broken */
     switch (pteam->topology) {
     case P_TOPOLOGY_FLAT:
         abs_coords.id += coords->id;
+        if (flags & P_COORDS_WRAP_ID) {
+            abs_coords.id %= pteam->size.id;
+            if (abs_coords.id < 0)
+                abs_coords.id += pteam->size.id;
+        }
         break;
     case P_TOPOLOGY_3D:
         abs_coords.plane += coords->plane;
-        if (flags & P_COORDS_WRAP_PLANE)
-            abs_coords.plane = abs_coords.plane % pteam->size.plane;
+        if (flags & P_COORDS_WRAP_PLANE) {
+            abs_coords.plane %= pteam->size.plane;
+            if (abs_coords.plane < 0)
+                abs_coords.plane += pteam->size.plane;
+        }
         /* Fall through */
     case P_TOPOLOGY_2D:
         abs_coords.row += coords->row;
-        if (flags & P_COORDS_WRAP_ROW)
-            abs_coords.row = abs_coords.row % pteam->size.row;
-        if (flags & P_COORDS_WRAP_COL)
-            abs_coords.col = abs_coords.col % pteam->size.col;
+        if (flags & P_COORDS_WRAP_ROW) {
+            abs_coords.row %= pteam->size.row;
+            if (abs_coords.row < 0)
+                abs_coords.row += pteam->size.row;
+        }
+        abs_coords.col += coords->col;
+        if (flags & P_COORDS_WRAP_COL) {
+            abs_coords.col %= pteam->size.col;
+            if (abs_coords.col < 0)
+                abs_coords.col += pteam->size.col;
+        }
         break;
     default:
         return -EINVAL;
