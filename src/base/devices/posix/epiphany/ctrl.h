abb935298263df384198f35377f07a8ac2e79216
Ola Jeppsson <ola@adapteva.com>
2016-08-04 08:43:24 +0200

    devices/posix/epiphany: Bump control structure size
    Allow devices size up to 4096 (simulator max).
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- src/base/devices/posix/epiphany/ctrl.h
+++ src/base/devices/posix/epiphany/ctrl.h
@@ -6,7 +6,7 @@
 #define STATUS_RUNNING 2   // Set by device in boilerplate.c:init()
 #define STATUS_DONE 3      // Set by device in boilerplate.c:fini()
 
-#define CTRL_MEM_SIZE 4096
+#define CTRL_MEM_SIZE ((sizeof(struct epiphany_ctrl_mem) + 4095) & ~4095)
 #define CTRL_MEM_OFFSET (0x2000000-CTRL_MEM_SIZE)
 #define CTRL_MEM_EADDR (0x90000000-CTRL_MEM_SIZE)
 #define ARGS_MEM_END_OFFSET CTRL_MEM_OFFSET
@@ -15,8 +15,8 @@
 #define EPIPHANY_DEV_MAX_ARGS_SIZE (15*1024*1024)
 
 struct epiphany_ctrl_mem {
-    uint32_t status[16]; // status field for team
-    struct loader_args loader_args[16];
+    uint32_t status[4096]; // status field for team
+    struct loader_args loader_args[4096];
 } __attribute__((packed)) __attribute__((aligned(4)));
                            // since this is referenced cross-architecture we
                            // need to pack struct explicitly to avoid surprises
